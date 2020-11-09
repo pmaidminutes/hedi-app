b06d0f543b6b53fc7a158af6fbbb4bb264c7b4af
woeishi <woei@refect.net>
2020-11-09 01:30:18 +0100

    [auth] enhanced error typeguard

--- modules/auth/requests/types.ts
+++ modules/auth/requests/types.ts
@@ -19,5 +19,7 @@ export interface IHTTPError {
 }
 
 export function IsIHTTPError(arg: any): arg is IHTTPError  {
+  if (!arg || typeof arg !== 'object')
+    return false;
   return ('code' in arg) && ('text' in arg);
 }
