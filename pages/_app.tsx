971b951c1ed47a00164d437627ee9770a3777e14
woeishi <woei@refect.net>
2020-11-24 10:37:18 +0100

    [devHack] speed up build by skipping scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import { AppProps } from 'next/app'
 import { Provider as AuthProvider } from 'next-auth/client';
-import './_app.scss';
+
+// HACK using precompiled default carbon css while not customizing styling to speed up build
+// switch '...carbon-components.min.css' with '..._app.scss' for applying styles 
+//import './_app.scss';
+import 'carbon-components/css/carbon-components.min.css';
 
 export default function HEDIApp({ Component, pageProps }: AppProps) {
   return (
