7d99571b5f83082ecd767657cab7adce7f9d01c1
woeishi <woei@refect.net>
2020-12-10 18:41:51 +0100

    [segments] path/props interfaces for generic route

--- common/types.ts
+++ common/types.ts
@@ -1,10 +1,24 @@
 import { ParsedUrlQuery } from "querystring";
+import { ITranslatable, ITypename, IURLPath } from "./model/cms";
 
 export interface ILanguageKey {
   lang: string;
 }
 
 export interface ILanguageParam extends ILanguageKey, ParsedUrlQuery {}
+
+export interface ISegmentParam extends ParsedUrlQuery {
+  segments?: string[];
+}
+
+export interface ISegmentPath {
+  params: ISegmentParam;
+  locale: string;
+}
+
+export interface ISegmentProps {
+  content: ITypename & { translations: (ITranslatable & IURLPath)[] };
+}
 
 export interface IHTTPError {
   code: number;
