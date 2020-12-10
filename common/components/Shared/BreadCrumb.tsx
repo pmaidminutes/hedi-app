b255014c9d86e82f5831584591bdaf5ddedcc3dc
pmaidminutes <priya.manoharan@gmail.com>
2020-12-10 16:27:53 +0100

    Updated breadcrumb to include sub paths

--- common/components/Shared/BreadCrumb.tsx
+++ common/components/Shared/BreadCrumb.tsx
@@ -1,5 +1,5 @@
-import { Breadcrumb, BreadcrumbItem } from "carbon-components-react";
 import { GetStaticProps } from "next";
+import { useRouter } from "next/router";
 
 export const getStaticProps: GetStaticProps<any> = async ({
   locale,
@@ -7,23 +7,61 @@ export const getStaticProps: GetStaticProps<any> = async ({
 }) => {
   return { props: { locales, locale } };
 };
-export interface HeaderProps {
-  pageTitle: string;
-  path: string;
+interface CrumbPath {
+  name: string;
+  url: string;
+  currentPage: boolean;
 }
-export const BreadCrumb: React.FunctionComponent<HeaderProps> = (
-  props: HeaderProps
+export interface BreadCrumbProps {
+  staticCrumb?: CrumbPath[];
+}
+export const BreadCrumb: React.FunctionComponent<BreadCrumbProps> = (
+  props: BreadCrumbProps
 ) => {
+  const router = useRouter();
+
+  const breadCrumbPath =
+    props.staticCrumb ?? constructUrl(router.asPath, router.locale ?? "de");
   return (
-    <>
-      <Breadcrumb noTrailingSlash>
-        <BreadcrumbItem>
-          <a href="/">{"HEDI"}</a>
-        </BreadcrumbItem>
-        <BreadcrumbItem href={props.path} isCurrentPage>
-          {props.pageTitle}
-        </BreadcrumbItem>
-      </Breadcrumb>
-    </>
+    <nav
+      className="by--row bx--row-padding bx--breadcrumb--no-trailing-slash pl-l-sm"
+      aria-label="breadcrumb">
+      <div className="bx--col bx--breadcrumb">
+        <div className="bx--breadcrumb-item">
+          <a href="/" className="bx--link">
+            Home
+          </a>
+        </div>
+        {breadCrumbPath.map((crumb, index) => (
+          <a
+            key={index}
+            href={crumb.url}
+            className={
+              crumb.currentPage
+                ? "bx--breadcrumb-item bx--breadcrumb-item--current bx--link"
+                : "bx--breadcrumb-item bx--breadcrumb-item bx--link"
+            }>
+            {crumb.name}
+          </a>
+        ))}
+      </div>
+    </nav>
   );
 };
+function constructUrl(asPath: string, locale: string): CrumbPath[] {
+  const pathArray = asPath.split("/");
+  const composedPath: CrumbPath[] = [];
+  let basePath = "/" + locale;
+  pathArray.map((path: string) => {
+    if (path.trim() != "") {
+      basePath = basePath + "/" + path;
+      composedPath.push({
+        name: path,
+        url: basePath,
+        currentPage: asPath.endsWith(path) ? true : false,
+      });
+    }
+  });
+
+  return composedPath;
+}
