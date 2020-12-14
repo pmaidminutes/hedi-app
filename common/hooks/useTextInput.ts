1d3cabbc444ade2adb0ded2d456678f34af8fea5
Nico Putzehl <nputzehl@aidminutes.com>
2020-12-14 19:34:11 +0100

    [auth-design] add useTextInput hook

---
+++ common/hooks/useTextInput.ts
@@ -0,0 +1,12 @@
+import {
+  ChangeEvent,
+  useState,
+} from "react";
+
+export function useTextInput(defaultText = "") {
+  const [value, setValue] = useState(defaultText);
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setValue(e.target.value);
+  }
+  return [value, handleChange] as const;
+}
