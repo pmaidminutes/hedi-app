190e67dbad5416eb71e3df32f00b9f3962e8c70b
Nico Putzehl <nputzehl@aidminutes.com>
2020-12-14 19:36:01 +0100

    [auth-design] mod LogOut component

--- common/components/Authentication/LogOut.tsx
+++ common/components/Authentication/LogOut.tsx
@@ -1,10 +1,26 @@
 import { logout } from "@/modules/auth/client";
-import { Button, FormGroup } from "carbon-components-react";
+import { FormGroup } from "carbon-components-react";
 
 export const LogOut = ({ label }: { label: string }) => (
   <FormGroup legendText={label ?? ""}>
-    <Button type="submit" value="Logout" onClick={logout} label>
-      Logout
-    </Button>
+    {/* HACK */}
+    <div style={{ position: "relative" }}>
+      <span
+        style={{
+          color: "#fff",
+          fontSize: "0.8em",
+          position: "absolute",
+          top: "-15px",
+          left: "-15px"
+        }}>
+        {label}
+      </span>
+      <button
+        className="bx--btn bx--btn--primary hedi-login-button"
+        type="button"
+        onClick={logout}>
+        Logout
+      </button>
+    </div>
   </FormGroup>
 );
