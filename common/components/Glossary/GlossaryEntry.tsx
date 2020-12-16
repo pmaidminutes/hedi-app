42ad9b19cab9f0288d1f7a2c5e591a07b3a2435b
Nico Putzehl <nputzehl@aidminutes.com>
2020-12-16 18:02:45 +0100

    [fix-glossary-linking] add a function to lowercase and get rid of german umlauts. clean up code a bit

--- common/components/Glossary/GlossaryEntry.tsx
+++ common/components/Glossary/GlossaryEntry.tsx
@@ -12,21 +12,28 @@ interface GlossaryProps {
   entryIndex: number;
 }
 
+function replaceUmlauts(str: string) {
+  return str
+    .toLowerCase()
+    .replace(/ä/g, "ae")
+    .replace(/ö/g, "oe")
+    .replace(/ü/g, "ue");
+}
+
 export const GlossaryEntry = (props: GlossaryProps) => {
-  const glossaryItem = props.glossaryItem;
-  const glossaryUrlTerm = props.glossaryUrlTerm;
+  const { glossaryItem, glossaryUrlTerm, defaultLocale } = props;
   return (
     <div className="bx--col-md-4">
       <ExpandableTile
         title={glossaryItem.label}
-        id={glossaryItem.label}
+        id={replaceUmlauts(glossaryItem.label)}
         tabIndex={0}>
         <TileAboveTheFoldContent>
           <div style={{ height: "100px" }}>
             <h2>{glossaryItem.label}</h2>
             {glossaryItem.translations.map(
               (translation: IGlossaryEntry, index) =>
-                translation.langcode === props.defaultLocale ? (
+                translation.langcode === defaultLocale ? (
                   <p key={index}>
                     <mark> {translation.label}</mark>
                   </p>
