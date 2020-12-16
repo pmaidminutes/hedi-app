79cafa50c227aaea914ad264957c8b41d59435ea
Nico Putzehl <nputzehl@aidminutes.com>
2020-12-16 14:56:27 +0100

    [category-colors] remove custom element in logout

--- common/components/Authentication/LogOut.tsx
+++ common/components/Authentication/LogOut.tsx
@@ -3,24 +3,11 @@ import { FormGroup } from "carbon-components-react";
 
 export const LogOut = ({ label }: { label: string }) => (
   <FormGroup legendText={label ?? ""}>
-    {/* HACK */}
-    <div style={{ position: "relative" }}>
-      <span
-        style={{
-          color: "#fff",
-          fontSize: "0.8em",
-          position: "absolute",
-          top: "-15px",
-          left: "-15px"
-        }}>
-        {label}
-      </span>
-      <button
-        className="bx--btn bx--btn--primary hedi-login-button"
-        type="button"
-        onClick={logout}>
-        Logout
-      </button>
-    </div>
+    <button
+      className="bx--btn bx--btn--primary hedi-login-button"
+      type="button"
+      onClick={logout}>
+      Logout
+    </button>
   </FormGroup>
 );
