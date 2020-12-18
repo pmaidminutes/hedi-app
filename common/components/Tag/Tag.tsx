3d0fb723808626b234cadc58a4f8ceaa77b39ea0
Nico Putzehl <nputzehl@aidminutes.com>
2020-12-18 17:12:22 +0100

    [tags] adjusting component to real content and linking tags to search.

--- common/components/Tag/Tag.tsx
+++ common/components/Tag/Tag.tsx
@@ -1,9 +1,12 @@
-export const Tag = ({ tagtext }: { tagtext: String }) => {
+import { ITag } from "@/common/model/cms";
+import Link from "next/link";
+
+export const Tag = ({ tag }: { tag: ITag }) => {
   return (
     <button className="bx--tag bx--tag--magenta">
-      <a className="bx--tag__label hedi-unstyled-link" href="#">
-        {tagtext}
-      </a>
+      <Link href={`/search/${tag.label}`} passHref>
+        <a className="bx--tag__label hedi-unstyled-link">{tag.label}</a>
+      </Link>
     </button>
   );
 };
