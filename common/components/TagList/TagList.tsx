6f89bef218c03173dd63c1c9d41652f0e258bdec
Nico Putzehl <nputzehl@aidminutes.com>
2020-12-18 17:11:52 +0100

    [tags] adjusting component to real content.

--- common/components/TagList/TagList.tsx
+++ common/components/TagList/TagList.tsx
@@ -1,12 +1,16 @@
+// TS
+import { ITags } from "@/common/model/cms";
+// Components
 import { Tag } from "../Tag";
-export const TagList = ({ tags }: { tags: String[] }) => {
+
+export const TagList = ({ tags }: ITags) => {
   return (
     <div className="bx--grid mb-l-sm">
       <h3 className="mt-s-md">Browse by tags</h3>
       <div className="bx--row mt-s-sm">
         <div className="bx--col-sm-4 bx--col-md-6 bx--col-lg-10">
-          {tags.map((tag, index) => (
-            <Tag tagtext={tag} key={index} />
+          {tags.map(tag => (
+            <Tag tag={tag} key={tag.id} />
           ))}
         </div>
       </div>
