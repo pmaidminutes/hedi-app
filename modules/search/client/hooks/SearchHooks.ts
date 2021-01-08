23de3228b9943f4b65acc7c5f20ab6afeb9dbaff
woeishi <woei@refect.net>
2021-01-08 14:13:22 +0100

    [search] add glossary type to search return

--- modules/search/client/hooks/SearchHooks.ts
+++ modules/search/client/hooks/SearchHooks.ts
@@ -3,6 +3,7 @@ import { IHTTPError } from "@/common/types";
 import { jsonFetcher } from "@/common/utils";
 import { IArticle } from "@/modules/editorial/article/types";
 import { ICategory } from "@/modules/editorial/category/types";
+import { IGlossaryTerm } from "@/modules/editorial/glossary/types/glossary";
 import { ISuggestEntry } from "../../types";
 
 export function useSearch(
@@ -12,7 +13,9 @@ export function useSearch(
 ) {
   const apiPath = "/api/" + lang + "/search/";
 
-  const swrResult = useSWR<IHTTPError | (IArticle | ICategory)[]>(
+  const swrResult = useSWR<
+    IHTTPError | (IArticle | ICategory | IGlossaryTerm)[]
+  >(
     searchText?.length > 3
       ? apiPath + encodeURI(searchText + "/" + entityType)
       : null,
