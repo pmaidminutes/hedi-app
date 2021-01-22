295bab491105c329659f97e8e61f08519184cc8b
Nico Putzehl <nputzehl@aidminutes.com>
2021-01-22 12:28:32 +0100

    [carbon-native-components] ->editorial->article->ArticleEntry

--- modules/editorial/article/client/components/ArticleEntry/ArticleEntry.tsx
+++ modules/editorial/article/client/components/ArticleEntry/ArticleEntry.tsx
@@ -1,7 +1,7 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { HTMLWithNextImage } from "@/modules/react/html";
 import { IArticleEntry } from "../../../types";
+import { ClickableTile } from "carbon-components-react";
 
 export const ArticleEntry = ({
   article,
@@ -12,18 +12,17 @@ export const ArticleEntry = ({
   const { locale, defaultLocale } = router;
   const { label, summary, route } = article;
   return (
-    <Link href={route} passHref>
-      <a href="#" className="bx--tile bx--tile--clickable hedi-unstyled-link">
-        <h4
-          dangerouslySetInnerHTML={{
-            __html: label,
-          }}></h4>
-        <HTMLWithNextImage
-          data={summary}
-          locale={locale === defaultLocale ? null : locale}
-        />
-      </a>
-    </Link>
+    <ClickableTile href={route}>
+      {/* TODO: check if h4 is right for hierachy */}
+      <h4
+        dangerouslySetInnerHTML={{
+          __html: label,
+        }}></h4>
+      <HTMLWithNextImage
+        data={summary}
+        locale={locale === defaultLocale ? null : locale}
+      />
+    </ClickableTile>
   );
 };
 [];
