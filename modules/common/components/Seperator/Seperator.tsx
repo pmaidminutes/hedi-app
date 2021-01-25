ab49be6a80f3f7b08338021c76b0a5e183908b01
Nico Putzehl <nputzehl@aidminutes.com>
2021-01-25 14:24:09 +0100

    [carbon-native-components] remove inline style from hr

--- modules/common/components/Seperator/Seperator.tsx
+++ modules/common/components/Seperator/Seperator.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Seperator = () => {
-  return <hr style={{ margin: "1.5rem 0", borderTop: "1px solid #dde1e6" }} />;
+  return <hr className="hedi-seperator" />;
 };
