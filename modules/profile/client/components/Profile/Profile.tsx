2fdb55ce96fc9565635592ec634894bf3238f70c
Nico Putzehl <nputzehl@aidminutes.com>
2021-01-26 11:09:50 +0100

    [profile-components] import subcomponents on profile

--- modules/profile/client/components/Profile/Profile.tsx
+++ modules/profile/client/components/Profile/Profile.tsx
@@ -1,9 +1,63 @@
 import { ICaregiver } from "../../../types";
+import { Address } from "../Address";
+import { Contact } from "../Contact";
+import { DetailedName } from "../DetailedName";
 
 interface IProfileProps {
   content: ICaregiver;
 }
 
-export const Profile = ({ content }: IProfileProps) => {
-  return <div></div>;
+export const Profile = (content: IProfileProps) => {
+  console.log({ content });
+  const {
+    street,
+    house_number,
+    postal_code,
+    city,
+    country,
+    county,
+    state,
+    district,
+    room,
+    phone,
+    phone_private,
+    mail,
+    website,
+    consultation_hours,
+    label,
+    display,
+    name,
+    surname,
+    suffix,
+    prefix,
+  } = content;
+  return (
+    <div>
+      <Address
+        addressdata={{
+          street,
+          house_number,
+          postal_code,
+          city,
+          country,
+          county,
+          state,
+          district,
+          room,
+        }}
+      />
+      <Contact
+        contactdata={{
+          phone,
+          phone_private,
+          mail,
+          website,
+          consultation_hours,
+        }}
+      />
+      <DetailedName
+        detailednamedata={{ label, display, name, surname, suffix, prefix }}
+      />
+    </div>
+  );
 };
