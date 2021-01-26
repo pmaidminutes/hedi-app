4ac2b6a828ccbdfe1c0fe6fb24c73d2320856c2e
Nico Putzehl <nputzehl@aidminutes.com>
2021-01-26 10:06:43 +0100

    [profile-components] add ProfilePage component

---
+++ modules/profile/client/components/Profile/Profile.tsx
@@ -0,0 +1,9 @@
+import { ICaregiver } from "../../../types";
+
+interface IProfileProps {
+  content: ICaregiver;
+}
+
+export const Profile = ({ content }: IProfileProps) => {
+  return <div></div>;
+};
