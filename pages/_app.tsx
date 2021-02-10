109f6e0583b0905d4a25637ff753b7331449a3dd
Nico Putzehl <nputzehl@aidminutes.com>
2021-02-10 16:12:39 +0100

    [cleanup-components] remove style.css and add cabon native css back.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 // HACK using precompiled default carbon css while not customizing styling to speed up build
 // switch '...carbon-components.min.css' with '..._app.scss' for applying styles
 //import './_app.scss';
-// import "carbon-components/css/carbon-components.min.css";
+import "carbon-components/css/carbon-components.min.css";
 import { Provider as AuthProvider } from "next-auth/client";
 import { AppProps } from "next/app";
-import "./styles.css";
+// import "./styles.css";
 
 export default function HEDIApp({ Component, pageProps }: AppProps) {
   return (
