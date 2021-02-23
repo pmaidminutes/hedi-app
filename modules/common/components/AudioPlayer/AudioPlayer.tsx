ddb0ae3695132573792a65bbb846c8961ce17414
Kathi Kierach <kkierach@aidminutes.com>
2021-02-23 12:35:56 +0100

    inital

--- modules/common/components/AudioPlayer/AudioPlayer.tsx
+++ modules/common/components/AudioPlayer/AudioPlayer.tsx
@@ -1,6 +1,6 @@
 export const AudioPlayer = ({ src }: { src?: string }) => {
   return (
-    <audio controls src={src} style={{ width: "100%", height: "30px" }}>
+    <audio controls src={src} className="hedi__audioplayer">
       {/* TODO add possibility of multiple types */}
       <source src={src} type="video/webm" />
       Your browser does not support the
