4eb3a12d1c51cc4ffdc8347858ee5d9c73a232bf
woeishi <woei@refect.net>
2021-03-02 02:02:12 +0100

    [react] useTextInput hook compatible to textarea

--- modules/react/hooks/useTextInput.ts
+++ modules/react/hooks/useTextInput.ts
@@ -2,7 +2,9 @@ import { ChangeEvent, useState } from "react";
 
 export function useTextInput(defaultText = "") {
   const [value, setValue] = useState(defaultText);
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+  function handleChange(
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
     setValue(e.target.value);
   }
   return [value, handleChange, setValue] as const;
