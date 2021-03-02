4ad59f50aebdba155bf3e6f80d1681b6efffd28b
woeishi <woei@refect.net>
2021-03-02 02:19:33 +0100

    [msg] login request typing

--- modules/messaging/request/login.ts
+++ modules/messaging/request/login.ts
@@ -1,6 +1,7 @@
-import { IMatrixClient } from "../types";
+import { MatrixClient } from "matrix-js-sdk";
 
-export async function login(msgClient: IMatrixClient) {
+export async function login(msgClient: MatrixClient) {
+  // TODO handle login failure gracefully
   const loginToken = await (await fetch("/api/msg/loginToken")).text();
   return msgClient.loginWithToken(loginToken);
 }
