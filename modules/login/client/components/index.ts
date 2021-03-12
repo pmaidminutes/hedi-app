313960094801897514ac1500dd3f54a429ffb8b7
woeishi <woei@refect.net>
2021-03-12 22:41:59 +0100

    [Login] static gen func and 'Try' component

--- modules/login/client/components/index.ts
+++ modules/login/client/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./LoginForm";
 export * from "./LoginView";
+export * from "./TryLogin";
---
+++ modules/login/server/generators/getStaticLoginViewProps.ts
@@ -0,0 +1,14 @@
+import { IAppPage } from "@/modules/common/types";
+import { segmentsToRoute } from "@/modules/common/utils";
+import { getLoginView } from "../../query";
+
+export const getStaticProps = async (
+  segments?: string[],
+  locale = "de"
+): Promise<IAppPage | null> => {
+  if (!segments) {
+    return null;
+  } else {
+    return getLoginView(segmentsToRoute([segments[0]], locale));
+  }
+};
