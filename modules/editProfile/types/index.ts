7fa6968a9b3ca4c8aa64626379b813c43b52c6ce
Nico Putzehl <nputzehl@aidminutes.com>
2021-03-15 10:26:30 +0100

    [editProfile-adjustments] export type

--- modules/editProfile/types/index.ts
+++ modules/editProfile/types/index.ts
@@ -2,3 +2,4 @@ export * from "./IEditProfileRequest";
 export * from "./IEditProfileError";
 export * from "./IEditProfileResponse";
 export * from "./IUpsertProfile";
+export * from "./IEditProfile"
