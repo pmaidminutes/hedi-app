9f5bfa33ce57b4e3be52c4c7a2b9e355c6a932e7
woeishi <woei@refect.net>
2021-03-17 04:10:11 +0100

    [login] fix segment to route error

--- modules/login/server/generators/getStaticLoginViewProps.ts
+++ modules/login/server/generators/getStaticLoginViewProps.ts
@@ -9,6 +9,6 @@ export const getStaticProps = async (
   if (!segments) {
     return null;
   } else {
-    return getLoginView(segmentsToRoute([segments[0]], locale));
+    return getLoginView(segmentsToRoute(segments, locale));
   }
 };
