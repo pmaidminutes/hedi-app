cc7f17636e8274c193a9ed1e87059aa077f72e98
Nico Putzehl <nputzehl@aidminutes.com>
2021-03-19 11:58:26 +0100

    [profile-view] add todos to profileEntry

--- modules/profile/client/components/ProfileEntry/ProfileEntry.tsx
+++ modules/profile/client/components/ProfileEntry/ProfileEntry.tsx
@@ -14,38 +14,26 @@ export const ProfileEntry = ({
   profile: IProfile | IMidwife | ICaregiver | IOrganisation | IInstitution;
 }): JSX.Element => {
   console.log({ profile });
-  const {
-    displayName,
-    postal_code,
-    city,
-    mail,
-    website,
-    phone,
-  } = profile;
+  const { displayName, postal_code, city, mail, website, phone } = profile;
 
   return (
     <>
-
       <section className="hedi__profile-entry">
         <Row>
           <Column lg={{ span: 4, offset: 1 }}>
-            <img
-              src="/images/profile_dummy.png"
-              alt="Profil Bild"
-              style={{
-
-              }}
-            />
+            {/* TODO image dynamisch */}
+            <img src="/images/profile_dummy.png" alt="Profil Bild" style={{}} />
           </Column>
           <Column lg={10}>
             <h2>{displayName}</h2>
+            {/* TODO subtitle? */}
             <h3>psychosoziale Beratung</h3>
             <address style={{}}>
               <p>
                 {postal_code} {city}
               </p>
               {/* TODO right number for phone linking */}
-              <a href={`tel:${phone}`}  target="_blank" title="Telefonnummer">
+              <a href={`tel:${phone}`} target="_blank" title="Telefonnummer">
                 {phone}
               </a>
               <a href={`mailto:${mail}`} target="_blank" title="E-Mail Address">
