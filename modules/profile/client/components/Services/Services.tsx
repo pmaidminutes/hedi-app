dfa9c8c6d3bdefdcb5f6a817f5dcd2c6b45a4dd8
Nico Putzehl <nputzehl@aidminutes.com>
2021-03-19 12:48:43 +0100

    [profile-view] add services component

---
+++ modules/profile/client/components/Services/Services.tsx
@@ -0,0 +1,26 @@
+import { ITag, TagType } from "@/modules/model";
+import { TagList } from "@/modules/common/components";
+import { Tile } from "carbon-components-react";
+
+interface IService {
+  tags?: ITag[];
+  headline?: string;
+  tagType?: TagType;
+}
+
+export const Services = ({
+  tags = [],
+  headline = "Headline",
+  tagType = "blue",
+}: IService) => {
+  return (
+    <section className="hedi__profile-services">
+      <Tile>
+        {/* TODO import as svg, alt*/}
+        <img src="/images/baby_on_hand.svg" alt="" />
+        {/* TODO title apppage */}
+        <TagList tags={tags} headline={headline} tagType={tagType}></TagList>
+      </Tile>
+    </section>
+  );
+};
