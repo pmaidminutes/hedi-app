52a7ce7c5cdbb008f327a7a4ad5f57f5eda65f78
Reza Bayat <reza.baiat88@hotmail.com>
2021-03-19 13:05:35 +0100

    [userfeedback] [thankyou-page] created

--- modules/common/query/getAppPages.ts
+++ modules/common/query/getAppPages.ts
@@ -1,6 +1,6 @@
-import { gql } from "@/modules/graphql";
+import { getServiceClient, gql, GQLEndpoint } from "@/modules/graphql";
 import { EntityLocalizedFields } from "@/modules/model";
-import { AppPageFields } from "../types/appPage";
+import { AppPageFields, IAppPage } from "../types/appPage";
 
 export const generateAppPagePathsGQL = (keys: string[]): string =>
   `${keys.join("_")}: appPagesByKey(keys: ${JSON.stringify(
@@ -8,3 +8,34 @@ export const generateAppPagePathsGQL = (keys: string[]): string =>
   )}, lang: $lang) { ${EntityLocalizedFields} }`;
 
 export const AppPagesGQL = gql`appPages(routes: $routes, lang: $lang) { ${AppPageFields} }`;
+
+export async function getAppPages(
+  keys: string[],
+  lang?: string
+): Promise<IAppPage[]> {
+  const query = gql`
+    query getAppPages (
+      $keys: [String!]!
+      $lang: String!
+      ) {
+        ${AppPageFields}
+      }
+  `;
+
+  const client = await getServiceClient(GQLEndpoint.Internal);
+  if (!client) return [];
+
+  return client
+    .request<IAppPage[]>(query, { keys, lang })
+    .catch(error => {
+      console.error("error", error);
+      return [];
+    });
+}
+
+export async function getAppPage(
+  key: string,
+  lang?: string
+): Promise<IAppPage> {
+  return getAppPages([key], lang).then(pages => pages[0] || null);
+}
