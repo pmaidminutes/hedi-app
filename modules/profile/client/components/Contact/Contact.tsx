7250912ed8643bb0c6f969f33a77e207a75edaeb
Nico Putzehl <nputzehl@aidminutes.com>
2021-03-19 13:49:35 +0100

    [profile-view] mod contact

--- modules/profile/client/components/Contact/Contact.tsx
+++ modules/profile/client/components/Contact/Contact.tsx
@@ -1,18 +1,54 @@
-import { IContact } from "@/modules/model";
-
+import { IContact, IProfile } from "@/modules/model";
+import { Tile } from "carbon-components-react";
 interface IContactProps {
-  content: IContact;
+  content: IProfile;
 }
 
 export const Contact = ({ content }: IContactProps): JSX.Element => {
-  const { phone, phone_private, mail, website, consultation_hours } = content;
+  const {
+    phone,
+    mail,
+    website,
+    consultation_hours,
+    street,
+    house_number,
+    city,
+    displayAddress,
+    postal_code,
+  } = content;
   return (
-    <section>
-      {phone ? <p>{phone}</p> : null}
-      {phone_private ? <p>{phone_private}</p> : null}
-      {mail ? <p>{mail}</p> : null}
-      {website ? <p>{website}</p> : null}
-      {consultation_hours ? <p>{consultation_hours}</p> : null}
+    <section className="hedi__profile-contact hedi__profile--tile">
+      <Tile>
+        <img src="/images/profile_pregnancy.svg" alt="" />
+        <address>
+          <p>{displayAddress}</p>
+          <p>
+            {street} {house_number}
+          </p>
+          <p>
+            {postal_code} {city}
+          </p>
+        </address>
+        {/* TODO right number for phone linking */}
+        <p>
+          <a href={`tel:${phone}`} target="_blank" title="Telefonnummer">
+            {phone}
+          </a>
+        </p>
+        <p>
+          <a href={`mailto:${mail}`} target="_blank" title="E-Mail Address">
+            {mail}
+          </a>
+        </p>
+        <p>
+          <a href={website} target="_blank" title="Webseite">
+            {website}
+          </a>
+        </p>
+        {/* TODO headline aus Appages */}
+        <h3>Sprechzeiten</h3>
+        {consultation_hours ? <p>{consultation_hours}</p> : null}
+      </Tile>
     </section>
   );
 };
