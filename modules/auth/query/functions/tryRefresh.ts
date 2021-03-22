ff4bfefe7f53658ea15d055550b8b630f9bacf1a
woeishi <woei@refect.net>
2021-03-22 19:38:18 +0100

    [auth] align refresh query return

--- modules/auth/query/functions/tryRefresh.ts
+++ modules/auth/query/functions/tryRefresh.ts
@@ -1,13 +1,14 @@
 import { getExpires } from "../utils";
 import { requestRefresh } from "../requests";
 import { IAuth } from "../../types";
+import { IsIHTTPError } from "@/modules/common/error";
 //https://next-auth.js.org/tutorials/refresh-token-rotation
 export async function tryRefresh<T extends IAuth>(auth: T) {
   if (Date.now() < auth.accessTokenExpires - 500) return auth;
 
   const { refreshToken, csrfToken } = auth;
   const tokenResp = await requestRefresh(refreshToken, csrfToken);
-  if (!tokenResp.access_token) return null;
+  if (IsIHTTPError(tokenResp)) return null;
 
   return {
     ...auth,
