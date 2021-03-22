1da5e85c5cc9747258cc623018c7fb2f5b43d6ce
Reza Bayat <reza.baiat88@hotmail.com>
2021-03-22 10:20:26 +0100

    [AppPageView] common view for AppPages

---
+++ modules/common/components/AppPage/AppPageView.tsx
@@ -0,0 +1,49 @@
+import { IAppPage } from "@/modules/common/types";
+import { Grid, Row, Column, AspectRatio } from "carbon-components-react";
+import Link from "next/link";
+import Image from "next/image";
+import { HTMLWithNextImage } from "@/modules/react/html";
+
+export const AppPageView = ({
+  content,
+  rootCssClass,
+}: {
+  content: IAppPage;
+  rootCssClass?: string;
+}) => {
+  return (
+    <div className={rootCssClass}>
+      {content.posterImage && (
+        <AspectRatio ratio="2x1">
+          <Image
+            src={content.posterImage.route}
+            alt={content.posterImage.alt}
+            className="hedi-header-image"
+            width={content.posterImage.width}
+            height={content.posterImage.height}
+          />
+        </AspectRatio>
+      )}
+      <Grid>
+        <Row>
+          <Column>
+            <h1>{content.longTitle ?? content.label}</h1>
+            <HTMLWithNextImage data={content.body} />
+            <div className="hedi-app-page-link-buttons">
+              {content.elements.map(element => (
+                <Link
+                  key={element.identifier + content.lang}
+                  href={"#"}
+                  passHref>
+                  <a href={"#" /* TODO redirect to correct location */}>
+                    {element.value}
+                  </a>
+                </Link>
+              ))}
+            </div>
+          </Column>
+        </Row>
+      </Grid>
+    </div>
+  );
+};
