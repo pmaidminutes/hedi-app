f606e20b0afe115fa9e1ba1103254035b85f4946
woeishi <woei@refect.net>
2021-03-22 02:29:41 +0100

    [editProfile] impl poster, title, body on view

--- modules/editProfile/components/EditProfile/EditProfile.tsx
+++ modules/editProfile/components/EditProfile/EditProfile.tsx
@@ -1,15 +1,31 @@
+import Image from "next/image";
 import { getUser } from "@/modules/auth/client";
+import { HTMLWithNextImage } from "@/modules/react/html";
 import { extractConfig, IEditProfileView } from "../../types";
 import { EditProfileForm, useEditProfileForm } from "../EditProfileForm";
 
 export const EditProfile = ({ content }: { content: IEditProfileView }) => {
   const [user] = getUser();
   if (!user) return null; //TODO senseful redirect
+  const poster = content.posterImage;
 
   return (
-    <EditProfileForm
-      config={extractConfig(content)}
-      {...useEditProfileForm(content.lang)}
-    />
+    <>
+      {!!poster && (
+        <Image
+          src={poster.route}
+          width={poster.width}
+          height={poster.height}
+          title={poster.label}
+          alt={poster.alt}
+        />
+      )}
+      <h1>{content.label}</h1>
+      <HTMLWithNextImage data={content.body} />
+      <EditProfileForm
+        config={extractConfig(content)}
+        {...useEditProfileForm(content.lang)}
+      />
+    </>
   );
 };
