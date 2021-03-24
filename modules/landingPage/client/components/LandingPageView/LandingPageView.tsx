81fa1f6cd33bfe05e80be7d185daa71e2027e544
Reza Bayat <reza.baiat88@hotmail.com>
2021-03-24 09:44:58 +0100

    [landingPage] show different Buttons if logged In

--- modules/landingPage/client/components/LandingPageView/LandingPageView.tsx
+++ modules/landingPage/client/components/LandingPageView/LandingPageView.tsx
@@ -1,3 +1,4 @@
+import { getUser } from "@/modules/auth/client";
 import { tryGet } from "@/modules/common/utils";
 import { ILandingPageView } from "@/modules/landingPage/types/ILandingPageView";
 import { SimplePageView } from "@/modules/simplePage/client/components";
@@ -10,17 +11,31 @@ import {
 } from "carbon-components-react";
 
 export const LandingPageView = ({ content }: { content: ILandingPageView }) => {
+  const [user] = getUser();
   return (
     <SimplePageView content={content}>
       <Row>
-        {content.links.map(link => (
-          <Column>
-            <ButtonSet stacked>
-              <FormLabel>{tryGet(link.key, content.elements)?.help}</FormLabel>
-              <Button href={link.route}>{link.label}</Button>
-            </ButtonSet>
-          </Column>
-        ))}
+        {user
+          ? content.linksIfLoggedIn.map(link => (
+              <Column>
+                <ButtonSet stacked>
+                  <FormLabel>
+                    {tryGet(link.key, content.elements)?.help}
+                  </FormLabel>
+                  <Button href={link.route}>{link.label}</Button>
+                </ButtonSet>
+              </Column>
+            ))
+          : content.links.map(link => (
+              <Column>
+                <ButtonSet stacked>
+                  <FormLabel>
+                    {tryGet(link.key, content.elements)?.help}
+                  </FormLabel>
+                  <Button href={link.route}>{link.label}</Button>
+                </ButtonSet>
+              </Column>
+            ))}
       </Row>
     </SimplePageView>
   );
