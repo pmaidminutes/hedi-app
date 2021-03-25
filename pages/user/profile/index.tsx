c5e4c028271388ba264ec3a0b68855aa0edd2758
woeishi <woei@refect.net>
2021-03-25 14:08:53 +0100

    [myProfile] integrate shell

--- pages/user/profile/index.tsx
+++ pages/user/profile/index.tsx
@@ -1,7 +1,12 @@
-import { getUser } from "@/modules/auth/client";
-import { getCurrentUserProfile } from "@/modules/profile/request/getCurrentUserProfile";
+import { GetStaticProps } from "next";
+
+import { IPageProps } from "@/modules/shell/types";
+import { getShell } from "@/modules/shell/query";
+import { useShell } from "@/modules/shell/hooks";
+import { Shell } from "@/modules/shell/components";
+
+import { useEffect } from "react";
 import { useRouter } from "next/router";
-import { tryGet } from "@/modules/common/utils";
 import {
   Button,
   ButtonSet,
@@ -9,42 +14,48 @@ import {
   FormLabel,
   Row,
 } from "carbon-components-react";
-import { useEffect } from "react";
-import { IAppPage } from "@/modules/common/types/appPage";
-import { GetStaticProps } from "next";
-import { getProfileStatic } from "@/modules/profile/query";
-import { Footer, Header } from "@/modules/shell/components";
-import { Content } from "carbon-components-react";
-import Head from "next/head";
 
-interface IMyProfileProps {
-  content: IAppPage;
-  locale: string;
-}
-export const getStaticProps: GetStaticProps<IMyProfileProps> = async ({
+import { IAppPage } from "@/modules/common/types/appPage";
+import { tryGet } from "@/modules/common/utils";
+import { getUser } from "@/modules/auth/client";
+import { getProfileStatic } from "@/modules/profile/query";
+import { getCurrentUserProfile } from "@/modules/profile/request/getCurrentUserProfile";
+
+export const getStaticProps: GetStaticProps<IPageProps<IAppPage>> = async ({
   locale,
 }) => {
-  locale = locale ?? "de";
-  const content = await getProfileStatic(locale);
+  const shellKeys = {
+    header: ["editprofile", "viewprofile", "profiles", "userfeedback"],
+    footer: ["imprint", "privacy"],
+  };
+
+  const [content, shellConfig] = await Promise.all([
+    getProfileStatic(locale ?? "de"),
+    getShell(locale, shellKeys),
+  ]);
+
   if (!content) {
     console.error("err");
     throw Error();
   }
 
+  const shell = useShell(content, shellConfig);
+
   return {
-    props: { content, locale },
+    props: { content, shell },
   };
 };
 
-export default function myProfile({ locale, content }: IMyProfileProps) {
+export default function myProfile(props: IPageProps<IAppPage>) {
+  const { content } = props;
   const router = useRouter();
   const [user, userIsLoading] = getUser();
   const [currentProfile, currentProfileIsLoading] = getCurrentUserProfile(
     user,
-    locale
+    content.lang
   );
   useEffect(() => {
-    if (!userIsLoading && !user) router.push("/" + locale);
+    if (!userIsLoading && !user) router.push("/" + content.lang);
     if (!currentProfileIsLoading && currentProfile)
       router.push(currentProfile.route);
   }, [
@@ -56,26 +67,19 @@ export default function myProfile({ locale, content }: IMyProfileProps) {
   const noProfileElement = tryGet("no_profile", content.elements);
 
   return (
-    <div>
-      <Head>
-        <title>HEDI - {content.label}</title>
-      </Head>
-      <Header {...content} />
-      <Content>
-        {!userIsLoading && user && (
-          <Row>
-            <Column>
-              <ButtonSet stacked>
-                <FormLabel>{noProfileElement?.description}</FormLabel>
-                <Button href={"/" + locale + "/user/profile/edit"}>
-                  {noProfileElement?.value}
-                </Button>
-              </ButtonSet>
-            </Column>
-          </Row>
-        )}
-      </Content>
-      <Footer />
-    </div>
+    <Shell {...props}>
+      {!userIsLoading && user && (
+        <Row>
+          <Column>
+            <ButtonSet stacked>
+              <FormLabel>{noProfileElement?.description}</FormLabel>
+              <Button href={"/" + content.lang + "/user/profile/edit"}>
+                {noProfileElement?.value}
+              </Button>
+            </ButtonSet>
+          </Column>
+        </Row>
+      )}
+    </Shell>
   );
 }
