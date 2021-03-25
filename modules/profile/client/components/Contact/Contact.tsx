ef81938e208dcbcd84c8dd06f4a7dd4f58f6eba9
Kathi Kierach <kkierach@aidminutes.com>
2021-03-25 11:24:26 +0100

    styled links

--- modules/profile/client/components/Contact/Contact.tsx
+++ modules/profile/client/components/Contact/Contact.tsx
@@ -1,5 +1,5 @@
 import { IContactProps, useContact } from "./useContact";
-import { Tile } from "carbon-components-react";
+import { Tile, Link } from "carbon-components-react";
 
 export const Contact = (props: IContactProps): JSX.Element => {
   const {
@@ -30,19 +30,33 @@ export const Contact = (props: IContactProps): JSX.Element => {
         {/* TODO right number for phone linking */}
 
         <p>
-          <a href={`tel:${phone}`} target="_blank" title="Telefonnummer">
+          <Link
+            href={`tel:${phone}`}
+            target="_blank"
+            title="Telefonnummer"
+            className="bx--link--lg">
             {phone}
-          </a>
+          </Link>
         </p>
         <p>
-          <a href={`mailto:${mail}`} target="_blank" title="E-Mail Address">
+          <Link
+            href={`mailto:${mail}`}
+            target="_blank"
+            title="E-Mail Address"
+            className="bx--link--lg"
+            inline>
             {mail}
-          </a>
+          </Link>
         </p>
         <p>
-          <a href={website} target="_blank" title="Webseite">
+          <Link
+            href={website}
+            target="_blank"
+            title="Webseite"
+            className="bx--link--lg"
+            inline>
             {website}
-          </a>
+          </Link>
         </p>
         <div className="hedi--consultation-hours-wrapper">
           {consultation_hours ? (
