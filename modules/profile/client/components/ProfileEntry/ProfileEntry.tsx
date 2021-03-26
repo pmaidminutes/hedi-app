30455a68fa0dc7c0ed5b7fbec24303e9806d7f3a
Nico Putzehl <nputzehl@aidminutes.com>
2021-03-26 17:40:18 +0100

    [profilelist-todos] linebreak domains

--- modules/profile/client/components/ProfileEntry/ProfileEntry.tsx
+++ modules/profile/client/components/ProfileEntry/ProfileEntry.tsx
@@ -35,7 +35,22 @@ export const ProfileEntry = (props: IProfileEntryProps): JSX.Element => {
             <div className="hedi--profile-entry-content">
               {/* TODO reuse contact */}
               <h2>{displayName}</h2>
-              <h3>{domains?.map(d => d.label).join(" & ")}</h3>
+              <h3>
+                {domains?.map((domain, index) => {
+                  console.log(index, domains.length);
+                  return (
+                    <>
+                      <span>{domain.label}</span>
+                      {domains.length > index + 1 ? (
+                        <span className="hedi--ampersand">
+                          {" "}
+                          & <br />
+                        </span>
+                      ) : null}
+                    </>
+                  );
+                })}
+              </h3>
               <address>
                 {postal_code} {city}
                 {/* TODO right number for phone linking */}
