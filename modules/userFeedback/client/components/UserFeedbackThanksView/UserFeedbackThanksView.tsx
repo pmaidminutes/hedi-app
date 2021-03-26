b21719e65580f3df15edc69491c888d792f98f3e
Nico Putzehl <nputzehl@aidminutes.com>
2021-03-26 12:42:31 +0100

    [design-profile-test] refactor UserFeedbackThanksView

--- modules/userFeedback/client/components/UserFeedbackThanksView/UserFeedbackThanksView.tsx
+++ modules/userFeedback/client/components/UserFeedbackThanksView/UserFeedbackThanksView.tsx
@@ -1,51 +1,26 @@
-import { getUser } from "@/modules/auth/client";
-import { IAppPage } from "@/modules/common/types";
-import { getCurrentUserProfile } from "@/modules/profile/request/getCurrentUserProfile";
 import { SimplePageView } from "@/modules/simplePage/client/components";
-import { getCurrentUserHasFeedback } from "@/modules/userFeedback/request/getCurrentUserHasFeedback";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { Button } from "carbon-components-react";
+import { ArrowLeft16 } from "@carbon/icons-react";
+import {
+  IUserFeedbackThanksProps,
+  useFeedbackThanksView,
+} from "./useFeedbackThanksView";
 
-export const UserFeedbackThanksView = ({ content }: { content: IAppPage }) => {
-  const [user, loading] = getUser();
-  const [currentProfile, currentProfileLoading] = getCurrentUserProfile(
-    user,
-    content.lang
-  );
-  const [hasFeedback, hasFeedbackLoading] = getCurrentUserHasFeedback(user);
+export const UserFeedbackThanksView = (props: IUserFeedbackThanksProps) => {
+  const { content, elements } = useFeedbackThanksView(props);
 
-  const router = useRouter();
-  useEffect(() => {
-    if (!loading && !user) router.push("/" + content.lang);
-    else if (
-      !currentProfileLoading &&
-      (!currentProfile || !currentProfile.route)
-    ) {
-      router.push("/" + content.lang + "/user/profile/edit");
-    } else if (!hasFeedbackLoading && hasFeedback !== true)
-      router.push("/" + content.lang + "/user/profile/feedback");
-  }, [
-    user,
-    loading,
-    currentProfile,
-    currentProfileLoading,
-    hasFeedback,
-    hasFeedbackLoading,
-  ]);
   return (
-    <SimplePageView
-      url="/Pregnancy_pink80.svg"
-      alt="Beschreibung des Bildes"
-      content={content}>
+    <SimplePageView url="/Pregnancy_pink80.svg" content={content}>
       <div className="hedi-app-page-link-buttons">
-        {content.elements.map(element => (
-          <Link
+        {elements.map(element => (
+          <Button
             key={element.identifier + content.lang}
-            href={"/" + content.lang + "/user/profile"}
-            passHref>
-            <a href={"/" + content.lang + "/user/profile"}>{element.value}</a>
-          </Link>
+            tooltip={element.value}
+            renderIcon={ArrowLeft16}
+            kind="ghost"
+            href={"/" + content.lang + "/user/profile"}>
+            {element.value}
+          </Button>
         ))}
       </div>
     </SimplePageView>
