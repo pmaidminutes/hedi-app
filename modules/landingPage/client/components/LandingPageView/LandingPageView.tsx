1de0da8981b6a8f55581a46a986c664f8f6b39e5
pmaidminutes <priya.manoharan@gmail.com>
2021-04-07 17:59:46 +0200

    updated urls

--- modules/landingPage/client/components/LandingPageView/LandingPageView.tsx
+++ modules/landingPage/client/components/LandingPageView/LandingPageView.tsx
@@ -10,7 +10,7 @@ export const LandingPageView = ({ content }: { content: ILandingPageView }) => {
   const { links, linksIfLoggedIn, isLastElement } = useLandingPage(content);
   return (
     <SimplePageView
-      url="/images/Pregnancy_blue.svg"
+      url={process.env.NEXT_PUBLIC_IMG}
       alt="Beschreibung des Bildes"
       content={content}>
       <Row>
