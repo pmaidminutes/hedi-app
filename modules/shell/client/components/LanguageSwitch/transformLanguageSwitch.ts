e5a3964112bd15aaf44827f303970000fa270ddf
Nico Putzehl <nputzehl@aidminutes.com>
2021-04-08 15:05:46 +0200

    [cleanup-client-components] shell - language switch

--- modules/shell/client/components/LanguageSwitch/transformLanguageSwitch.ts
+++ modules/shell/client/components/LanguageSwitch/transformLanguageSwitch.ts
@@ -1,19 +1,22 @@
 import { IEntity, IUIElementTexts } from "@/modules/model";
 import { VerticalDirection } from "carbon-components-react/typings/shared";
+import { tryGet } from "@/modules/common/utils";
 
 export interface ILanguageSwitchProps {
   direction?: VerticalDirection;
   links: IEntity[];
   config?: IUIElementTexts[];
 }
-export function useLanguageSwitch({
+export function transformLanguageSwitch({
   links,
   config,
   direction = "bottom",
 }: ILanguageSwitchProps) {
+  const tooltip = tryGet("menu_language", config)?.value;
   return {
     links,
     direction,
     config,
+    tooltip,
   };
 }
