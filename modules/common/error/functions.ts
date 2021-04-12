1daaa80f0f21f6f1b9cd3a7efc86d9029b9f71e0
woeishi <woei@refect.net>
2021-04-12 05:58:17 +0200

    [error] helper funcs for IHTTPError unions

--- modules/common/error/functions.ts
+++ modules/common/error/functions.ts
@@ -1,4 +1,4 @@
-import { IHTTPError } from "./types";
+import { IHTTPError, IsIHTTPError } from "./types";
 
 export function responseToIHTTPError(
   response: Response,
@@ -9,4 +9,36 @@ export function responseToIHTTPError(
     status,
     message: message ?? statusText,
   };
+}
+
+type WithIHTTPError<T, E extends IHTTPError> = T | E;
+
+export function logAndNull<T, E extends IHTTPError>(obj: WithIHTTPError<T, E>) {
+  if (IsIHTTPError(obj)) {
+    console.error(obj);
+    return null;
+  }
+  return obj as Exclude<T | E, E>;
+}
+
+export function logAndFallback<T, E extends IHTTPError, U>(
+  obj: WithIHTTPError<T, E>,
+  fallback: T
+) {
+  if (IsIHTTPError(obj)) {
+    console.error(obj);
+    return fallback;
+  }
+  return obj as Exclude<T | E, E>;
+}
+
+export function throwOnError<T, E extends IHTTPError>(
+  obj: WithIHTTPError<T, E>
+) {
+  if (IsIHTTPError(obj)) {
+    const err = new Error(obj.message ?? `HTTP ${obj.status} error`);
+    err.message = JSON.stringify(obj);
+    throw err;
+  }
+  return obj;
 }
