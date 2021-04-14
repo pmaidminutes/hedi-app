62badfcfa8d4b280a01cc17b3a666adfee81a21d
Nico Putzehl <nputzehl@aidminutes.com>
2021-04-14 16:23:16 +0200

    [shell-layout-integration] add layout viewprofile

--- modules/profile/client/components/ViewProfile/ViewProfile.tsx
+++ modules/profile/client/components/ViewProfile/ViewProfile.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import { Button, Column, Row, Loading } from "carbon-components-react";
 
 import { ITyped } from "@/modules/model";
-import { SimplePageView } from "@/modules/simplePage/client/components";
 
 import { IViewProfileView } from "../../../types";
 import { getEditProfileLink } from "./getEditProfileLink";
@@ -21,11 +20,7 @@ export const ViewProfileView = ({ content }: { content: IViewProfileView }) => {
   const { currentProfileIsLoading } = useViewProfile(content.lang);
   const editProfile = getEditProfileLink(content);
   return (
-    <SimplePageView
-      url={process.env.NEXT_PUBLIC_IMG_HEADER_PROFILE}
-      alt="Beschreibung des Bildes"
-      content={content}
-      rightColumnProps={{ md: 4, lg: 6, xlg: 6 }}>
+    <>
       {currentProfileIsLoading ? (
         <Loading />
       ) : (
@@ -37,6 +32,6 @@ export const ViewProfileView = ({ content }: { content: IViewProfileView }) => {
           </Column>
         </Row>
       )}
-    </SimplePageView>
+    </>
   );
 };
