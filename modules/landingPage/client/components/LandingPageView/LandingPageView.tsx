98ca4adbb3f78a9d770c0735f5dcadb3464bc886
Nico Putzehl <nputzehl@aidminutes.com>
2021-04-14 14:22:07 +0200

    [shell-layout-integration] del console log

--- modules/landingPage/client/components/LandingPageView/LandingPageView.tsx
+++ modules/landingPage/client/components/LandingPageView/LandingPageView.tsx
@@ -9,8 +9,6 @@ export const LandingPageView = ({ content }: { content: ILandingPageView }) => {
   const { links, linksIfLoggedIn, isLastElement } = transformLandingPage(
     content
   );
-
-  console.log({user})
   return (
 
       <Row>
