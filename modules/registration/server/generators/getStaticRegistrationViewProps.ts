9f3479941be33a8b282f11cbc2ce0ed223a347b7
Nico Putzehl <nputzehl@aidminutes.com>
2021-04-14 15:07:26 +0200

    [shell-layout-integration] add layout for registration

--- modules/registration/server/generators/getStaticRegistrationViewProps.ts
+++ modules/registration/server/generators/getStaticRegistrationViewProps.ts
@@ -1,4 +1,5 @@
 import { segmentsToRoute } from "@/modules/common/utils";
+import { ILayout } from "@/modules/shell/client/components/Layout/types";
 import { IPageConfig } from "@/modules/shell/types";
 import { getRegistrationView } from "../../query";
 import { IRegistrationView } from "../../types";
@@ -13,8 +14,24 @@ export const getStaticProps = async (
 
   if (!content) return null;
 
+  const layoutImg = {
+    alt: "Beschreibung",
+    src: `${process.env.NEXT_PUBLIC_IMG_HEADER}`,
+  };
+  
+  const layout: ILayout = {
+    pageLayout: "imageAndColumn",
+    layoutImg,
+    rightColumnProps: { md: 4, lg: 6, xlg: 6 },
+  };
+
+  const shell: IPageConfig = {
+    useHeader: "AUTHORIZED",
+    layout,
+  };
+
   return {
     ...content,
-    useHeader: "AUTHORIZED",
+    ...shell,
   };
 };
