bc34043cf5dee110a4536bae54545c356ae70931
Nico Putzehl <nputzehl@aidminutes.com>
2021-04-14 10:27:04 +0200

    [shell-layout-integration] add Layout component

---
+++ modules/shell/client/components/Layout/Layout.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import { transformLayout, ILayout } from "./transformLayout";
+import { SingleColumn, ImageAndColumn } from "./templates";
+
+export const Layout: React.FC<ILayout> = props => {
+  const {
+    left,
+    layoutImg,
+    right,
+    posterImgSrc,
+    posterImage,
+    wrapperClass,
+    pageLayout,
+    condensed,
+    narrow,
+    body,
+    headline,
+    children,
+    groupClass,
+  } = transformLayout(props);
+
+  return (
+    <div className={wrapperClass}>
+      {posterImage && (
+        <Image
+          className="hedi--hero-image"
+          src={posterImgSrc}
+          alt={posterImage.alt}
+          width={posterImage.width}
+          height={posterImage.height}
+          layout="responsive"
+        />
+      )}
+      {pageLayout === "imageAndColumn" ? (
+        <ImageAndColumn
+          headline={headline}
+          body={body}
+          groupClass={groupClass}
+          left={left}
+          right={right}
+          layoutImg={layoutImg}
+        />
+      ) : (
+        <SingleColumn
+          headline={headline}
+          body={body}
+          groupClass={groupClass}
+          narrow={narrow}
+          condensed={condensed}>
+          {children}
+        </SingleColumn>
+      )}
+    </div>
+  );
+};
