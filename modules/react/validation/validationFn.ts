f320420a62ac7f5e902974528461b403683d56e8
Samira Mokhtari <mokhtari.9310@yahoo.com>
2021-04-20 15:55:11 +0200

    [fixed]ValidationFn

--- modules/react/validation/validationFn.ts
+++ modules/react/validation/validationFn.ts
@@ -1,13 +1,13 @@
 export const requiredValidationFn = <T>() => {
   return (value: T) => !!value;
 };
-export const minLengthValidationFn = <T>(minLength: number) => {
-  return (value: T) => new Object(value ?? "").toString().length >= minLength;
+export const minLengthValidationFn = (minLength: number) => {
+  return (value: string) => value.length >= minLength;
 };
-export const maxLengthValidationFn = <T>(maxLength: number) => {
-  return (value: T) => new Object(value ?? "").toString().length <= maxLength;
+export const maxLengthValidationFn = (maxLength: number) => {
+  return (value: string) => value.length <= maxLength;
 };
 
-export const regexValidationFn = <T>(regex: RegExp) => {
-  return (value: T) => regex.test(new Object(value ?? "").toString());
+export const regexValidationFn = (regex: RegExp) => {
+  return (value: string) => regex.test(value);
 };
