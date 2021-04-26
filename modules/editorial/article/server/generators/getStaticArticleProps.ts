aee6f714266f61035652b5106517b1cf61989f4e
Nico Putzehl <nputzehl@aidminutes.com>
2021-04-26 15:55:00 +0200

    [reintegrate-editorial] mod article generator

--- modules/editorial/article/server/generators/getStaticArticleProps.ts
+++ modules/editorial/article/server/generators/getStaticArticleProps.ts
@@ -1,14 +1,30 @@
 import { segmentsToRoute } from "@/modules/common/utils";
 import { IArticle } from "../../types";
 import { getArticle } from "../../query";
+import { IPageConfig } from "@/modules/shell/types";
+import { ILayout } from "@/modules/shell/client/components/Layout/types";
 
 export const getStaticProps = async (
   segments?: string[],
   locale = "de"
-): Promise<IArticle | null> => {
+): Promise<(IArticle & IPageConfig) | null> => {
   if (!segments) {
     return null;
-  } else {
-    return getArticle(segmentsToRoute(segments, locale));
   }
+  const content = await getArticle(segmentsToRoute(segments, locale));
+
+  if (!content) return null;
+
+  const layout: ILayout = {
+    pageLayout: "editorial",
+  };
+
+  const shell: IPageConfig = {
+    useHeader: true,
+    layout,
+  };
+  return {
+    ...content,
+    ...shell,
+  };
 };
--- modules/shell/client/components/Layout/templates/index.ts
+++ modules/shell/client/components/Layout/templates/index.ts
@@ -1,2 +1,3 @@
 export * from "./SingleColumn";
 export * from "./ImageAndColumn";
+export * from "./Editorial";
