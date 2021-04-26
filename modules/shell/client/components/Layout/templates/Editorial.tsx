35a27a5c271b8f19fc5655f9a86d1a9bcd23533c
Nico Putzehl <nputzehl@aidminutes.com>
2021-04-26 15:59:52 +0200

    [reintegrate-editorial] mod editorial template

--- modules/shell/client/components/Layout/templates/Editorial.tsx
+++ modules/shell/client/components/Layout/templates/Editorial.tsx
@@ -2,12 +2,15 @@ import { Column, Grid, Row } from "carbon-components-react";
 import React from "react";
 import { ILayoutBasicTemplate } from "../types";
 
-export const Editorial: React.FC<ILayoutBasicTemplate> = ({ children }) => {
+export const Editorial: React.FC<ILayoutBasicTemplate> = ({
+  children,
+  groupClass,
+}) => {
   return (
     <Grid>
       <Row>
         <Column sm={4} md={{ span: 6, offset: 1 }} lg={{ span: 8, offset: 4 }}>
-          {children}
+          {children && <div className={groupClass}>{children}</div>}
         </Column>
       </Row>
     </Grid>
