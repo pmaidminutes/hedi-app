9c6a8f6d6b9a3670790a1552bd3af1b17bb0bfc4
Nico Putzehl <nputzehl@aidminutes.com>
2021-04-27 11:58:27 +0200

    [refactor-getstaticprops-route] mod getProfilePage

--- modules/profile/server/pages/getProfilePage.ts
+++ modules/profile/server/pages/getProfilePage.ts
@@ -1,17 +1,12 @@
-import { getLangByRoute, segmentsToRoute } from "@/modules/common/utils";
+import { getLangByRoute } from "@/modules/common/utils";
 import { getProfile } from "../../query";
 import { getProfileDefinition } from "../../query/getProfileDefinition";
 import { ProfileView } from "../../types";
 
 export const getProfilePage = async (
-  segments?: string[],
-  locale = "de"
+  route: string
 ): Promise<ProfileView | null> => {
-  if (!segments) return null;
-  const route = segmentsToRoute(segments, locale);
-  const definition = await getProfileDefinition(
-    getLangByRoute(route) ?? locale
-  );
+  const definition = await getProfileDefinition(getLangByRoute(route) ?? "de");
   const content = await getProfile(route);
 
   if (!content || !definition) return null;
