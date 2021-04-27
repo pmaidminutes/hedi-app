af0092a855dee43eb06d66045827de0630a504ad
Nico Putzehl <nputzehl@aidminutes.com>
2021-04-27 12:03:52 +0200

    [refactor-getstaticprops-route] mod getUserFeedbackThanksPage

--- modules/userFeedback/server/pages/getUserFeedbackThanksPage.ts
+++ modules/userFeedback/server/pages/getUserFeedbackThanksPage.ts
@@ -1,4 +1,3 @@
-import { IAppPage } from "@/modules/common/types";
 import { segmentsToRoute } from "@/modules/common/utils";
 import { ILayout } from "@/modules/shell/client/components/Layout/types";
 import { IPageConfig } from "@/modules/shell/types";
@@ -6,15 +5,12 @@ import { getUserFeedbackThanksView } from "../../query";
 import { IUserFeedbackThanksView } from "../../types/IUserFeedbackThanksView";
 
 export const getUserFeedbackThanksPage = async (
-  segments?: string[],
-  locale = "de"
+  route: string
 ): Promise<(IUserFeedbackThanksView & IPageConfig) | null> => {
-  if (!segments) return null;
+  const content = await getUserFeedbackThanksView(route);
 
-  const content = await getUserFeedbackThanksView(
-    segmentsToRoute(segments, locale)
-  );
   if (!content) return null;
+
   const layoutImg = {
     src: `${process.env.NEXT_PUBLIC_IMG_HEADER_SIMPLE}`,
     alt: "Some description",
@@ -26,7 +22,7 @@ export const getUserFeedbackThanksPage = async (
   };
   const shell: IPageConfig = {
     useHeader: "AUTHORIZED",
-    redirectUnAuthorized: "/" + locale,
+    redirectUnAuthorized: "/" + content.lang,
     layout,
   };
 
