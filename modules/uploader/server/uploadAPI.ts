d2365774ae350b107e06ea034b862523a160d50f
Reza Bayat <reza.baiat88@hotmail.com>
2021-04-27 11:36:35 +0200

    [upload] moved getGuid to helpers

--- modules/uploader/server/uploadAPI.ts
+++ modules/uploader/server/uploadAPI.ts
@@ -11,13 +11,7 @@ import {
   IUploadedFileInfo,
 } from "./extractPostedData";
 import { saveBufferToDisk } from "./saveTools";
-
-const getGuid = () =>
-  "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    const r = (Math.random() * 16) | 0;
-    const v = c == "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
+import { getGuid } from "./helpers";
 
 // TODO under development
 export const uploadAPI: NextApiHandler<IUploadResult[]> = async (req, res) => {
