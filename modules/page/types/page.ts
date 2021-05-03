ff6dcfd4ea006833b42d2e527f1ab951bc7cedc1
woeishi <woei@refect.net>
2021-05-03 12:22:34 +0200

    [page] ts types and gql query fields

---
+++ modules/page/types/page.ts
@@ -0,0 +1,45 @@
+import { gql } from "@/modules/graphql";
+import {
+  IEntityLocalized,
+  EntityLocalizedFields,
+  EntityTranslatedFields,
+  IEntityTranslated,
+} from "@/modules/model";
+import { Component } from "@/modules/model/components";
+
+export interface IPageEntry extends IEntityLocalized {
+  id: string;
+  description?: string;
+}
+
+export const PageEntryGQL = gql`... on Page {
+${EntityLocalizedFields}
+id
+description
+}`;
+
+export const isIPageEntry = (obj: any): obj is IPageEntry =>
+  obj != null && obj?.type === "Page";
+
+export enum PageVisibility {
+  Public,
+  LoggedIn,
+  User,
+  Internal,
+}
+
+export interface IPage extends IEntityTranslated<IEntityLocalized> {
+  id: string;
+  visibility: PageVisibility;
+  components: Component[];
+}
+
+export const PageGQL = gql`... on Page {
+  ${EntityTranslatedFields}
+  id
+  visibility
+  components
+}`;
+
+export const isIPage = (obj: any): obj is IPage =>
+  obj != null && obj?.type === "Page";
