7e76ff427bfca5c70ef80522ef1d98af472beece
Samira Mokhtari <mokhtari.9310@yahoo.com>
2021-05-12 11:46:11 +0200

    [ConsultationHours]convert string to object

--- modules/editProfile/client/request/upsertProfile.ts
+++ modules/editProfile/client/request/upsertProfile.ts
@@ -10,5 +10,16 @@ export function upsertProfile(
   return fetch(url, {
     method: "POST",
     body: JSON.stringify(data),
-  }).then(res => res.json());
+  })
+    .then(res => res.json())
+    .then(res => {
+      //HACK temp code. after changing consultationHours from string to object, this code should be deleted
+      const result = res as IUpsertProfile;
+      if (result.success && result.profile) {
+        result.profile.consultationHours = JSON.parse(
+          result.profile?.consultation_hours ?? "[]"
+        );
+      }
+      return result;
+    });
 }
