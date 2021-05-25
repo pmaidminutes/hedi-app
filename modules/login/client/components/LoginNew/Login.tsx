653f5510398a2cec33c1cd595ccf9a1a99abe954
Nico Putzehl <nputzehl@aidminutes.com>
2021-05-25 10:53:03 +0200

    [pages] add loginnew for test purpose

---
+++ modules/login/client/components/LoginNew/Login.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { ILogin, transformLogin } from "./transformLogin";
+import {
+  TextInput,
+  ButtonHintLink,
+  Notification,
+  Button,
+} from "@/modules/common/components";
+import { Column, Form, Row } from "carbon-components-react";
+import { ArrowLeft16 } from "@carbon/icons-react";
+
+export const Login = ({ content }: { content: ILogin }) => {
+  const {
+    username,
+    password,
+    success,
+    invalid,
+    submit,
+    register,
+    back,
+  } = transformLogin(content);
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    console.log("Submit");
+  };
+
+  return (
+    <>
+      <Row>
+        <Column>
+          <Form onSubmit={handleSubmit}>
+            {username && <TextInput {...username} />}
+            {password && <TextInput {...password} />}
+            {invalid && <Notification {...invalid} />}
+            {success && <Notification {...success} />}
+            <div className="hedi--login-buttoncontainer">
+              {submit && register && (
+                <ButtonHintLink
+                  button={submit}
+                  link={register}
+                  text="No account yet?"
+                />
+              )}
+            </div>
+          </Form>
+        </Column>
+      </Row>
+      <Row>
+        <Column>{back && <Button renderIcon={ArrowLeft16} {...back} />}</Column>
+      </Row>
+    </>
+  );
+};
---
+++ modules/login/client/components/LoginNew/transformLogin.ts
@@ -0,0 +1,23 @@
+import {
+  findButtonInstance,
+  findLinkInstance,
+  findNotificationInstance,
+  findTextInputInstance,
+} from "@/modules/model/components";
+import { IPage } from "@/modules/page/types";
+
+export interface ILogin extends IPage {}
+export function transformLogin(props: ILogin) {
+  const { components } = props;
+
+  const username = findTextInputInstance(components, "username");
+  const password = findTextInputInstance(components, "password");
+  const success = findNotificationInstance(components, "success");
+  const invalid = findNotificationInstance(components, "invalid");
+  const submit = findButtonInstance(components, "submit");
+  const register = findLinkInstance(components, "register");
+  const back = findButtonInstance(components, "back");
+
+  console.log({ props });
+  return { username, password, success, invalid, submit, register, back };
+}
