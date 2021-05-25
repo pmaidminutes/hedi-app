8515b972d3e00787fc6c30e67a964c6f87a7619a
Nico Putzehl <nputzehl@aidminutes.com>
2021-05-25 10:47:07 +0200

    [pages]  add textinput component

---
+++ modules/common/components/TextInput/TextInput.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { TextInput as ITextInput } from "@/modules/model/components";
+import { TextInputProps } from "carbon-components-react";
+import { TextInput as CarbonTextInput } from "carbon-components-react";
+
+export const TextInput = (props: ITextInput & TextInputProps) => {
+  console.log({ props });
+  const {
+    kind,
+    labelText,
+    placeholder,
+    id,
+    helperText,
+    type,
+    ariaLabel,
+  } = props;
+  return (
+    <CarbonTextInput
+      {...props}
+      aria-label={ariaLabel || ""}
+      // id={id}
+      // labelText={labelText ?? ""}
+      // placeholder={placeholder}
+      // helperText={helperText}
+      // type={type}
+    />
+  );
+};
--- modules/common/components/index.ts
+++ modules/common/components/index.ts
@@ -1,8 +1,14 @@
 // TODO remove once a dedicated media module exists
 export * from "./AudioPlayer";
 export * from "./BgImgContainer";
+export * from "./Button";
 export * from "./ButtonContainer";
+export * from "./ButtonHintText";
+export * from "./Label";
+export * from "./Link";
+export * from "./Notification";
 export * from "./Seperator";
+export * from "./TextInput";
 export * from "./Rating";
 export * from "./Tag";
 export * from "./TagList";
