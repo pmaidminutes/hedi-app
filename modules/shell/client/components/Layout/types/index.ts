1b1c2bb1b5c4acb61fa83c0047cabc3aefb34dd0
Nico Putzehl <nputzehl@aidminutes.com>
2021-05-25 10:57:36 +0200

    fixup! [pages] mod types

--- modules/shell/client/components/Layout/types/index.ts
+++ modules/shell/client/components/Layout/types/index.ts
@@ -1,5 +1,6 @@
 import { IAppPage } from "@/modules/common/types";
 import { Image } from "@/modules/model/components";
+import { IPage } from "@/modules/page/types";
 import { ColumnDefaultProps } from "carbon-components-react";
 import React from "react";
 
@@ -21,7 +22,7 @@ export interface ILayoutImage {
   alt?: string;
 }
 export interface ILayoutProps extends ILayout {
-  content: IAppPage;
+  content: IAppPage & IPage;
   children: React.ReactNode;
   layout: ILayout;
 }
