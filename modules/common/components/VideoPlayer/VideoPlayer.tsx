b4361433d571c606a15ca9f427b9409131a1a72b
Nico Putzehl <nputzehl@aidminutes.com>
2021-05-26 22:07:07 +0200

    [pages] mod videoplayer

--- modules/common/components/VideoPlayer/VideoPlayer.tsx
+++ modules/common/components/VideoPlayer/VideoPlayer.tsx
@@ -1,70 +1,8 @@
-import { useRouter } from "next/router";
-import React, { useRef, useState, useEffect } from "react";
-import videojs from "video.js";
-import "video.js/dist/video-js.css";
+import React from "react";
+import { IVideoPlayerProps, useVideo } from "./useVideo";
 
-// Needs data like this
-// {
-//   subtitles: [
-//     { label: "Deutsch", langcode: "de", src: "/subtitle/de.vtt" },
-//     { label: "English", langcode: "en", src: "/subtitle/en.vtt" },
-//     { label: "Espagnol", langcode: "es", src: "/subtitle/es.vtt" },
-//   ],
-//   sources: [
-//     { mimeType: "mp4", src: "/sintel-short.mp4" },
-//     { mimeType: "webm", src: "/sintel-short.webm" },
-//   ],
-// }
-
-interface IVideoPlayer {
-  data: {
-    sources: ISources[];
-    subtitles: ISubtitles[];
-  };
-}
-
-interface ISources {
-  mimeType: string;
-  src: string;
-}
-interface ISubtitles {
-  label: string;
-  langcode: string;
-  src: string;
-}
-
-export const VideoPlayer = ({ data }: IVideoPlayer): JSX.Element => {
-  const router = useRouter();
-  const { locale } = router;
-  const { sources, subtitles } = data;
-
-  const videoPlayerRef = useRef(null); // Instead of ID
-  const [currentTime, setCurrentTime] = useState(0);
-
-  const videoJSOptions = {
-    autoplay: "muted",
-    controls: true,
-    userActions: { hotkeys: true },
-    playbackRates: [0.5, 1, 1.5, 2],
-    language: locale,
-    aspectRatio: "16:9",
-  };
-
-  useEffect(() => {
-    if (videoPlayerRef) {
-      const player = videojs(videoPlayerRef.current, videoJSOptions, () => {
-        player.on("ended", () => {
-          console.log("ended");
-        });
-        player.on("timeupdate", () => {
-          setCurrentTime(player.currentTime());
-        });
-        console.log("Player Ready");
-      });
-    }
-
-    return () => {};
-  }, []);
+export const VideoPlayer = (props: IVideoPlayerProps): JSX.Element => {
+  const { subtitles, videoPlayerRef, mimeType, src } = useVideo(props);
 
   return (
     <div data-vjs-player>
@@ -72,21 +10,17 @@ export const VideoPlayer = ({ data }: IVideoPlayer): JSX.Element => {
         style={{ width: "100%" }}
         ref={videoPlayerRef}
         className="video-js">
-        {sources.map((source, index) => (
-          <source
-            src={source.src}
-            key={index}
-            type={`video/${source.mimeType}`}
-          />
-        ))}
-        {subtitles.map((subtitle, index) => (
-          <track
-            label={subtitle.label}
-            key={`subtitle-${index}`}
-            kind="subtitles"
-            srcLang={subtitle.langcode}
-            src={subtitle.src}></track>
-        ))}
+        <source src={src} type={mimeType} />
+
+        {subtitles &&
+          subtitles.map((subtitle, index) => (
+            <track
+              label={subtitle.label}
+              key={`subtitle-${index}`}
+              kind="subtitles"
+              srcLang={subtitle.langcode}
+              src={subtitle.src}></track>
+          ))}
       </video>
     </div>
   );
