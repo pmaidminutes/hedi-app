12c87df59815bfba691475cb6ba4eae87db97953
woeishi <woei@refect.net>
2021-05-26 16:01:00 +0200

    [model-components] add labelTextEnd to datepicker

--- modules/model/components/DatePicker.ts
+++ modules/model/components/DatePicker.ts
@@ -11,6 +11,7 @@ export interface DatePicker extends IComponent {
   placeholder?: string;
   minDate?: string;
   maxDate?: string;
+  labelTextEnd?: HTML;
 }
 
 export const isDatePicker = (obj: IComponent): obj is DatePicker =>
