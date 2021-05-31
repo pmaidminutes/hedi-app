e07b6a8459dd50ade49807237f500d4a9a4e2f24
woeishi <woei@refect.net>
2021-05-31 02:26:01 +0200

    [profile] union of personal & prof as IUserProfile

--- modules/profile/types/index.ts
+++ modules/profile/types/index.ts
@@ -3,6 +3,7 @@ export * from "./IProfile";
 export * from "./IPersonal";
 export * from "./IBusinessProfile";
 export * from "./IProfessional";
+export * from "./IUserProfile";
 export * from "./IAssociation";
 export * from "./IViewProfileView";
 export * from "./ProfileType";
