b536431e64763214436eee21e0ca45f6654ab7cb
woeishi <woei@refect.net>
2021-06-01 00:07:24 +0200

    [profile] webLink molecule

--- modules/profile/client/components/index.ts
+++ modules/profile/client/components/index.ts
@@ -3,6 +3,7 @@ export * from "./Contact";
 export * from "./Address";
 export * from "./PhoneLink";
 export * from "./EmailLink";
+export * from "./WebLink";
 export * from "./ProfileEntry";
 export * from "./ProfileList";
 export * from "./ViewProfile";
