dd509f7f7a306e166db3c75838a9e66accdb3c71
woeishi <woei@refect.net>
2021-06-01 00:06:16 +0200

    [profile] PhoneLink molecule

--- modules/profile/client/components/index.ts
+++ modules/profile/client/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./Profile";
 export * from "./Contact";
 export * from "./Address";
+export * from "./PhoneLink";
 export * from "./ProfileEntry";
 export * from "./ProfileList";
 export * from "./ViewProfile";
