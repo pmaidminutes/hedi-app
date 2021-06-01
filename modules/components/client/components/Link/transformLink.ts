e40ed75cb6c09a5089a97b813b10772de2fa545e
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-01 16:29:45 +0200

    [pages-login] components fixes

--- modules/components/client/components/Link/transformLink.ts
+++ modules/components/client/components/Link/transformLink.ts
@@ -3,6 +3,10 @@ import { HTML } from "@/modules/react/html/HTML";
 import { LinkPropsBase } from "carbon-components-react/lib/components/UIShell/Link";
 export interface ILinkProps extends ILink, Omit<LinkPropsBase, "id"> {}
 export function transformLink(props: ILinkProps) {
-  const { labelText, ...rest } = props;
-  return { labelText: HTML({ data: labelText }), ...rest };
+  const { labelText, ariaLabel, ...rest } = props;
+  return {
+    labelText: HTML({ data: labelText }),
+    "aria-label": ariaLabel,
+    ...rest,
+  };
 }
