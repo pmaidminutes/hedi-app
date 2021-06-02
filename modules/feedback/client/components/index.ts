fec319a3ef3860ea98360643884cd7acdc734753
Reza Bayat <reza.baiat88@hotmail.com>
2021-06-02 10:27:37 +0200

    [feedback] removing feedbackEntry and putting into feedbackInput

--- modules/feedback/client/components/index.ts
+++ modules/feedback/client/components/index.ts
@@ -1,7 +1,6 @@
 export * from "./TryFeedback";
 export * from "./FeedbackView";
 export * from "./MultipleFeedback/MultipleFeedback";
-export * from "./FeedbackEntry/FeedbackEntry";
 export * from "./FeedbackForm/FeedbackForm";
 export * from "./FeedbackSendbox/FeedbackSendbox";
 export * from "./FeedbackThanksView";
