41af2ad6b49a72d82b63c18042f81517aefd1095
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-03 17:29:37 +0200

    [shellconfig] mod the getShell function

--- modules/shell/query/getShell.ts
+++ modules/shell/query/getShell.ts
@@ -1,55 +1,29 @@
-import { logAndFallback, logAndNull } from "@/modules/common/error";
-import { IAppPage } from "@/modules/common/types";
+import { logAndNull } from "@/modules/common/error";
 import { gql, serviceGQuery } from "@/modules/graphql";
-import { WithUIElementsFields } from "@/modules/model";
+import { IPage, PageGQL } from "@/modules/page/types";
 import { IShell } from "../types";
-import { ShellLinkFields } from "../types/shellLinks";
 import { LanguagesGQL } from "./getLanguages";
 
-export const getShellLinksGQL = (name: string, keys: string[]) => {
-  return `${name}:appPagesByKey(keys:[${keys
-    .map(key => `"${key}"`)
-    .join(",")}],lang: $lang) {
-    ${ShellLinkFields}
-    key
-  }`;
-};
-
-export async function getShell(
-  lang: string = "de",
-  // TODO besser typen
-  linkKeys: Record<string, string[]>
-): Promise<IShell> {
-  const gqlQueries = Object.entries(linkKeys).map(([key, value]) =>
-    getShellLinksGQL(key, value)
-  );
-  gqlQueries.push(LanguagesGQL);
-
+export async function getShell(lang: string = "de"): Promise<IShell> {
   const query = gql`
     query getShell(
         $lang: String!
         $includeSelf: Boolean
     ) {
-      shellAppPage:appPagesByKey(keys:["shellConfigs"], lang: $lang){${WithUIElementsFields}}
-      ${gqlQueries.join("\n")}
+      shellPage:pagesById(ids:["shellConfigs"], lang: $lang){${PageGQL}}
+      ${LanguagesGQL}
     }
   `;
-  return serviceGQuery<
-    Omit<IShell, "shellConfig"> & { shellAppPage: IAppPage[] }
-  >(query, { lang }).then(data => {
+  return serviceGQuery<Omit<IShell, "shellConfig"> & { shellPage: IPage[] }>(
+    query,
+    { lang }
+  ).then(data => {
     const result = logAndNull(data);
     if (!result) return { links: [], languages: [], shellConfig: [] };
-    const { shellAppPage, ...rest } = result;
+    const { shellPage, ...rest } = result;
     return {
-      shellConfig: shellAppPage?.[0].elements ?? [],
+      shellConfig: shellPage?.[0].components ?? [],
       ...rest,
     } as IShell;
   });
-  // rest umbauen
 }
-
-// const typeAsLink = (array: IEntity[]) => {
-//   return array.map(element => {
-//     return { type: "Link", route: element.route, label: element.label };
-//   });
-// };
