a82dc1fa7dde9156f341905c9dac03dbc4f0dc09
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-08 17:51:52 +0200

    [component-headline] add headline model

---
+++ modules/components/types/Headline.ts
@@ -0,0 +1,22 @@
+import { HTML, IComponent } from "./Component";
+
+export type HeadlineKind = "Headline";
+
+export interface IHeadlineComponent extends IComponent {
+  kind: HeadlineKind;
+  labelKind: string;
+  text?: HTML;
+}
+
+export const isHeadline = (obj: IComponent): obj is IHeadlineComponent =>
+  obj?.kind === "Headline";
+
+export const isHeadlineInstance = (
+  obj: IComponent,
+  id: string
+): obj is IHeadlineComponent => isHeadline(obj) && obj.id === id;
+
+export const findHeadlineInstance = (array: IComponent[], id: string) => {
+  const element = array.filter(isHeadline).find(item => item.id === id);
+  return element;
+};
