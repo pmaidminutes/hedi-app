b2fc19ab425cc6b963300d5f492dc4b926098e2f
Samira Mokhtari <mokhtari.9310@yahoo.com>
2021-06-09 16:13:31 +0200

    [ValidationTestInput] test login page

--- modules/auth/client/components/Login/Login.tsx
+++ modules/auth/client/components/Login/Login.tsx
@@ -2,11 +2,19 @@ import React, { SyntheticEvent } from "react";
 import { transformLogin } from "./transformLogin";
 import { ILogin } from "../../../types";
 import { useLogin } from "./useLogin";
-import { TextInput, Button, InlineNotification } from "@/modules/components";
+import {
+  TextInput,
+  Button,
+  InlineNotification,
+  useValidationSummary,
+  requiredValidationFn,
+  minLengthValidationFn,
+  ValidationTextInput,
+  ValidationSummary,
+} from "@/modules/components";
 import { ButtonHintLink } from "@/modules/common/components";
 import { Column, Form, Row, Loading } from "carbon-components-react";
 import { ArrowLeft16 } from "@carbon/icons-react";
-
 export const Login = ({ content }: { content: ILogin }) => {
   const {
     username,
@@ -28,14 +36,32 @@ export const Login = ({ content }: { content: ILogin }) => {
     isSuccess,
     isLoggedIn,
   } = useLogin(redirectUrl);
-
+  const { validationErrors, setValidationError } = useValidationSummary();
   return !isLoading ? (
     <>
       <Row>
         <Column>
           <Form onSubmit={(e: any) => handleLogin(e)}>
-            {username && !isLoggedIn && <TextInput {...username} />}
-            {password && !isLoggedIn && <TextInput {...password} />}
+            {username && !isLoggedIn && (
+              <ValidationTextInput
+                validateFn={[requiredValidationFn()]}
+                enableValidation
+                onValidation={texterror =>
+                  setValidationError("username", texterror)
+                }
+                {...username}
+              />
+            )}
+            {password && !isLoggedIn && (
+              <ValidationTextInput
+                validateFn={[requiredValidationFn(), minLengthValidationFn(3)]}
+                enableValidation
+                onValidation={texterror =>
+                  setValidationError("password", texterror)
+                }
+                {...password}
+              />
+            )}
             {invalid && hasError && <InlineNotification {...invalid} />}
             {success && isSuccess && <InlineNotification {...success} />}
             {!isLoggedIn ? (
@@ -58,6 +84,8 @@ export const Login = ({ content }: { content: ILogin }) => {
             />
           )}
         </Column>
+
+        <ValidationSummary validationErrors={validationErrors} />
       </Row>
     </>
   ) : (
