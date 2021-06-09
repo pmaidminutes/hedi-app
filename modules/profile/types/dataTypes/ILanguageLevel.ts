ccb8eec1e56f07cb4be1e3985f0b1e807599050d
woeishi <woei@refect.net>
2021-06-10 01:57:39 +0200

    [profile] languagelevel to input fn

--- modules/profile/types/dataTypes/ILanguageLevel.ts
+++ modules/profile/types/dataTypes/ILanguageLevel.ts
@@ -12,3 +12,10 @@ export interface ILanguageLevelInput {
   langcode: string;
   level: number;
 }
+
+export function languageLevelToInput(
+  languageLevel: ILanguageLevel
+): ILanguageLevelInput {
+  // NOTE will change, since ILanguageLevel is just a temporary model until we have the language list
+  return languageLevel;
+}
