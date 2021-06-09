b515f4a02563c5f2820ca2c144d88fd0a031ceaa
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-09 15:43:41 +0200

    [rework-editorial-pages] mod catgory page and entry

--- modules/editorial/category/client/components/Category/Category.tsx
+++ modules/editorial/category/client/components/Category/Category.tsx
@@ -3,20 +3,25 @@ import { Seperator } from "@/modules/common/components";
 import { ArticleEntry } from "@/modules/editorial/article/client/components";
 import { Row, Column } from "carbon-components-react";
 import { transformCategory, ICategoryProps } from "./transformCategory";
-
 export const Category = (props: ICategoryProps): JSX.Element => {
   const { articles, categories } = transformCategory(props);
+  console.log({ props });
   return (
     <>
       {categories ? (
         <>
-          <h3 className="mt-l-sm">Topic subcategories</h3>
           <Row>
-            {categories.map(category => (
-              <Column key={category.route}>
-                <CategoryEntry category={category} />
-              </Column>
-            ))}
+            <Column {...{ sm: 2, md: 4, lg: 8, xlg: 8 }}>
+              <Row>
+                {categories.map(category => (
+                  <Column
+                    {...{ sm: 2, md: 4, lg: 8, xlg: 8 }}
+                    key={category.route}>
+                    <CategoryEntry category={category} />
+                  </Column>
+                ))}
+              </Row>
+            </Column>
           </Row>
         </>
       ) : null}
--- modules/editorial/category/server/page/getCategoryPage.ts
+++ modules/editorial/category/server/page/getCategoryPage.ts
@@ -5,8 +5,15 @@ import { ILayout } from "@/modules/shell/client/components/Layout/types";
 export const getCategoryPage = async (
   content: ICategory
 ): Promise<ICategory & IPageConfig> => {
+  const layout: ILayout = {
+    pageLayout: "editorial",
+    posterImage: content.image || null,
+    breadcrumbs: {...content}
+  };
   const shell: IPageConfig = {
-    useHeader: "AUTHORIZED",
+    useHeader: true,
+    layout,
+
   };
 
   return {
