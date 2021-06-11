3b59d9f8bb2a243b2fe14afd38f6426917b31eb3
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-11 12:48:09 +0200

    [map] code cleanup

--- modules/map/client/components/Map/Map.tsx
+++ modules/map/client/components/Map/Map.tsx
@@ -1,5 +1,3 @@
-//import 'leaflet-defaulticon-compatibility'
-import { LatLngTuple } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
--- modules/map/client/components/Map/useMap.ts
+++ modules/map/client/components/Map/useMap.ts
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { IMap } from "@/modules/map/types";
-import L from "leaflet";
 
 export function useMap(props: IMap) {
   const { locations } = props;
@@ -12,6 +11,5 @@ export function useMap(props: IMap) {
   useEffect(() => {
     setHasLocations(locations && locations.length > 0);
   }, [locations])
-  console.log({hasLocations})
   return { hasLocations };
 }
