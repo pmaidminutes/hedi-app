bcbc9b2fce0454984e1120e18f2ae068b742305c
woeishi <woei@refect.net>
2021-06-11 20:15:37 +0200

    [components] TextInput Wrapper type fix

--- modules/components/client/components/TextInput/TextInput.tsx
+++ modules/components/client/components/TextInput/TextInput.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { ITextInputComponent } from "../../../types";
-import { TextInputProps } from "carbon-components-react";
 import { TextInput as CarbonTextInput } from "carbon-components-react";
-import { transformTextInput } from "./transformTextInput";
+import { ITextInputProps, transformTextInput } from "./transformTextInput";
 
-export const TextInput = (props: ITextInputComponent & TextInputProps) => {
+export const TextInput = (props: ITextInputProps) => {
   const data = transformTextInput(props);
   return <CarbonTextInput {...data} />;
 };
