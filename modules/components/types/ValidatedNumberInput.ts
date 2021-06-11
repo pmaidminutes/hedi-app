13632244a94c98c0ff6d15ea22a72be9d654842d
Samira Mokhtari <mokhtari.9310@yahoo.com>
2021-06-11 13:08:34 +0200

    [ValidationNumberInput] change interface

--- modules/components/types/ValidatedNumberInput.ts
+++ modules/components/types/ValidatedNumberInput.ts
@@ -2,7 +2,7 @@ import { IValidationFunction } from "@/modules/react/validation";
 import { NumberInputProps } from "carbon-components-react";
 import { INumberInputProps } from "../client/components/NumberInput/transformNumberInput";
 
-export interface IValidatedNumberInputProps extends NumberInputProps {
+export interface IValidatedNumberInputProps extends INumberInputProps {
   validateFn?: IValidationFunction | IValidationFunction[];
   enableValidation?: Boolean;
   onValidation?: (textError: string | string[]) => void;
