022a1bdffad6e90f3efae3e346307b5282820754
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-15 10:34:14 +0200

    [fuzzy-filter-dropdown] add common component

--- modules/common/components/index.ts
+++ modules/common/components/index.ts
@@ -3,6 +3,7 @@ export * from "../../components/client/components/AudioPlayer";
 export * from "./BgImgContainer";
 export * from "./ButtonContainer";
 export * from "./ButtonHintText";
+export * from "./FuzzyFilterDropdown";
 export * from "./Seperator";
 export * from "./Rating";
 export * from "./SelectFieldVisibility";
