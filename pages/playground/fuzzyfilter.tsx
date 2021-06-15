5dbde5c4f56c3a3b6be4c99a64567a9a1878b8d0
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-15 10:34:22 +0200

    [fuzzy-filter-dropdown]  add playground page

---
+++ pages/playground/fuzzyfilter.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import { useState } from "react";
+
+export default function FuzzyFilterPlayground() {
+  return (
+    <div>
+      <Head>
+        <title>Fuzzy Filter</title>
+      </Head>
+      <h1>Fuzzy Filter</h1>
+    </div>
+  );
+}
