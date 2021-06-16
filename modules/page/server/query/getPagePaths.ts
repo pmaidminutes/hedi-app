2eb461627ee51a534bcac99dd31657816c77bd8a
woeishi <woei@refect.net>
2021-06-16 15:31:40 +0200

    [pages] use specialized gql query for not pulling internal paths

--- modules/page/server/query/getPagePaths.ts
+++ modules/page/server/query/getPagePaths.ts
@@ -1,4 +1,4 @@
 import { gql } from "@/modules/graphql";
 import { EntityLocalizedFields } from "@/modules/model";
 
-export const PagePathsGQL = gql`pages(lang: $lang) { ${EntityLocalizedFields} }`;
+export const PagePathsGQL = gql`renderPages(lang: $lang) { ${EntityLocalizedFields} }`;
