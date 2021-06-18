21f128d0af04c31f330a42f8786d428356f5fb5e
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-18 10:56:06 +0200

    [field-access-component] make onChange optional

--- modules/common/components/SelectFieldVisibility/transformSelectFieldVisibility.ts
+++ modules/common/components/SelectFieldVisibility/transformSelectFieldVisibility.ts
@@ -2,11 +2,15 @@ import { ISelectComponent } from "@/modules/components";
 
 export interface ISelectFieldVisibilty extends ISelectComponent {
   value?: number;
-  onChange: (value: number) => void;
+  onChange?: (value: number) => void;
 }
 
 export function transformSelectFieldVisibilty(props: ISelectFieldVisibilty) {
   const { value, items, onChange } = props;
 
-  return { items, value: value || 0, onChange };
+  return {
+    items,
+    value: value || 0,
+    onChange: onChange || (() => console.log("Function missing")),
+  };
 }
