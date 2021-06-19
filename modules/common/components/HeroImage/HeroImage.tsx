bad0096619379c163f4d4d8f9a827ffb720558fa
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-19 13:38:29 +0200

    [heroimage-component] add className

--- modules/common/components/HeroImage/HeroImage.tsx
+++ modules/common/components/HeroImage/HeroImage.tsx
@@ -6,7 +6,7 @@ import { IImageComponent } from "@/modules/components";
 export const HeroImage = (props: IImageComponent) => {
   const { image, color } = transformHeroImage(props);
   return (
-    <div style={{ backgroundColor: color }}>
+    <div style={{ backgroundColor: color }} className="hedi--hero-image">
       <Grid className="hedi--grid__no-padding">
         <Image
           className="hedi--hero-image__image"
