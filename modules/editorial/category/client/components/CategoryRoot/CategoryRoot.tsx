a200959eff8485d0fb6fe6dd246520bd4fe908dd
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-21 12:49:53 +0200

    [main-category-card] mod Grid in CategoryRoot

--- modules/editorial/category/client/components/CategoryRoot/CategoryRoot.tsx
+++ modules/editorial/category/client/components/CategoryRoot/CategoryRoot.tsx
@@ -8,39 +8,32 @@ import { Label, Body } from "@/modules/components";
 import { SearchInput } from "@/modules/search/client/components";
 import { MainCategoryCardList } from "../MainCategoryCardList";
 export const CategoryRoot = (props: ICategoryRoot) => {
-  console.log({ props });
   const { categories, articles, headline, text } = transformCategoryRoot(props);
   return (
     <section>
+      {/* TODO spacing and check, why Headline got a p tag */}
+      {headline && (
+        <Row>
+          <Column md={{ span: 4 }}>
+            <Label {...headline} />
+          </Column>
+        </Row>
+      )}
+      {text && (
+        <Row>
+          <Column md={{ span: 4 }}>
+            <Body {...text} />
+          </Column>
+        </Row>
+      )}
       <Row>
-        <Column lg={{ span: 14, offset: 1 }} className="hedi--col--grid-nest">
-          <Grid>
-            {/* TODO spacing and check, why Headline got a p tag */}
-            {headline && (
-              <Row>
-                <Column md={{ span: 4 }}>
-                  <Label {...headline} />
-                </Column>
-              </Row>
-            )}
-            {text && (
-              <Row>
-                <Column md={{ span: 4 }}>
-                  <Body {...text} />
-                </Column>
-              </Row>
-            )}
-            <Row>
-              <Column md={{ span: 4, offset: 4 }}>
-                <SearchInput
-                  onQueryChanged={() => console.log("query changed")}
-                />
-              </Column>
-            </Row>
-          </Grid>
+        <Column md={{ span: 4, offset: 4 }}>
+          <SearchInput onQueryChanged={() => console.log("query changed")} />
         </Column>
       </Row>
+
       <MainCategoryCardList {...props} />
+
       {/* MainCategoryCardList */}
       {/* {categories.map((category, index) => (
         <CategoryEntry
