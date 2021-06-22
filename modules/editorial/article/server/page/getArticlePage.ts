1a1edf921174cbdf12baa01d69566fbcf851cc07
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-22 19:03:23 +0200

    [article-page] add article Layout template

--- modules/editorial/article/server/page/getArticlePage.ts
+++ modules/editorial/article/server/page/getArticlePage.ts
@@ -1,18 +1,21 @@
 import { IArticle } from "../../types";
 import { IPageConfig } from "@/modules/shell/types";
 import { ILayout } from "@/modules/shell/client/components/Layout/types";
+import { imageToImageComponent } from "@/modules/components";
 
 export const getArticlePage = async (
   content: IArticle
 ): Promise<IArticle & IPageConfig> => {
+  const posterImage = content.category.image;
   const layout: ILayout = {
-    pageLayout: "editorial",
+    pageLayout: "article",
     singleColumnProps: {
       sm: 4,
       md: { span: 6, offset: 1 },
-      lg: { span: 8, offset: 4 },
+      lg: { span: 10, offset: 2 },
     },
     breadcrumbs: { ...content },
+    posterImage: imageToImageComponent(posterImage),
   };
 
   const shell: IPageConfig = {
