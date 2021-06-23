2f88b4e110aa5c19653afb71f8068ff3dfbc1c11
woeishi <woei@refect.net>
2021-06-24 00:47:01 +0200

    [profile] upsertOrganisation api & handler

--- modules/profile/types/APIUrls.ts
+++ modules/profile/types/APIUrls.ts
@@ -2,3 +2,4 @@ export const profileListAPIUrl = "/api/profiles";
 export const userProfileAPIUrl = "/api/user/profile";
 export const userProfileRouteAPIUrl = "/api/user/profile/route";
 export const upsertProfessionalAPIUrl = "/api/user/profile/upsertProfessional";
+export const upsertOrganisationAPIUrl = "/api/user/profile/upsertOrganisation";
