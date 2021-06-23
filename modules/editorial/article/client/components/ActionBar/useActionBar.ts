6649ba1da9887f7c9ca293134c857caeee4d7f9a
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-23 10:45:30 +0200

    [article-page] mod ActionBar

--- modules/editorial/article/client/components/ActionBar/useActionBar.ts
+++ modules/editorial/article/client/components/ActionBar/useActionBar.ts
@@ -1,31 +1,20 @@
-import { buildAssetUrl } from "@/modules/common/utils";
-import { IAudio } from "@/modules/editorial/types";
-
 import { useState, useEffect } from "react";
 import { IArticleAction } from "../../../types";
 
 export interface IActionBarProps {
   actions?: IArticleAction[];
-  audio?: IAudio;
 }
 
 export function useActionBar(props: IActionBarProps) {
-  const { actions, audio } = props;
-  const [hasActionBar, setHasActionBar] = useState(actions! || audio);
-
-  // const audioSrc = audio ? buildAssetUrl(audio.route) : null;
-  // TODO remove when using real audio
-  // const audioSrc = "/dummy/audio/audio.mp3";
+  const { actions } = props;
+  const [hasActionBar, setHasActionBar] = useState(!!actions);
 
   useEffect(() => {
-    setHasActionBar(actions! || audio);
-  }, [actions, audio]);
+    setHasActionBar(!!actions);
+  }, [actions]);
 
   return {
     hasActionBar,
     actions: actions === undefined ? null : actions,
-    hasAudio: true,
-    // hasAudio: audio === undefined ? false : true,
-    // audioSrc,
   };
 }
