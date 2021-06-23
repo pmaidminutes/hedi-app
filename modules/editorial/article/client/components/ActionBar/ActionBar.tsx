39410ee0eed6fe2900c126186214938fbab856e6
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-23 09:30:43 +0200

    [article-page] mod action bar item

--- modules/editorial/article/client/components/ActionBar/ActionBar.tsx
+++ modules/editorial/article/client/components/ActionBar/ActionBar.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { useActionBar, IActionBarProps } from "./useActionBar";
 import { Button } from "carbon-components-react";
-import { AudioPlayer } from "@/modules/common/components";
+import { AudioPlayer, Seperator } from "@/modules/common/components";
+import {
+  VolumeUp16,
+  Share16,
+  Bookmark16,
+  Printer16,
+  Language16,
+} from "@carbon/icons-react";
 
 export const ActionBar = (props: IActionBarProps) => {
   const { hasActionBar, actions, hasAudio } = useActionBar(props);
@@ -9,8 +16,30 @@ export const ActionBar = (props: IActionBarProps) => {
   if (!hasActionBar) return null;
   return (
     <div className="hedi--action-bar">
+      <div className="hedi--action-bar__items">
+        <div className="hedi--action-bar__item">
+          <VolumeUp16 />
+          Artikel anhören
+        </div>
+        <div className="hedi--action-bar__item">
+          <Share16 />
+          Teilen
+        </div>
+        <div className="hedi--action-bar__item">
+          <Bookmark16 />
+          Lesezeichen
+        </div>
+        <div className="hedi--action-bar__item">
+          <Printer16 />
+          Drucken
+        </div>
+        <div className="hedi--action-bar__item">
+          <Language16 />
+          Sprachen ändern
+        </div>
+      </div>
       {/* {hasAudio && <AudioPlayer src={audioSrc ?? ""} />} */}
-      {actions &&
+      {/* {actions &&
         actions.map((action, index) => (
           <Button
             key={action.description + index}
@@ -19,7 +48,8 @@ export const ActionBar = (props: IActionBarProps) => {
             hasIconOnly
             onClick={() => action.handler()}
           />
-        ))}
+        ))} */}
+      <Seperator style="dashed" color="blue" />
     </div>
   );
 };
