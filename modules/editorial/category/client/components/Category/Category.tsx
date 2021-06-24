969991e043a764516002950e59023e4d0ac5d32f
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-24 12:02:12 +0200

    [article-entry-list] mod Category * use ArticleEntryList * add hook to check if category has subcategories

--- modules/editorial/category/client/components/Category/Category.tsx
+++ modules/editorial/category/client/components/Category/Category.tsx
@@ -3,8 +3,11 @@ import { Seperator } from "@/modules/common/components";
 import { ArticleEntry } from "@/modules/editorial/article/client/components";
 import { Row, Column } from "carbon-components-react";
 import { transformCategory, ICategoryProps } from "./transformCategory";
+import { ArticleEntryList } from "@/modules/editorial/article/client";
+import { useSubCategories } from "./useSubCategories";
 export const Category = (props: ICategoryProps): JSX.Element => {
   const { articles, categories } = transformCategory(props);
+  const { hasSubCategories } = useSubCategories(categories);
   return (
     <>
       {categories ? (
@@ -28,13 +31,10 @@ export const Category = (props: ICategoryProps): JSX.Element => {
       {articles ? (
         <>
           <Seperator />
-          <Row>
-            {articles?.map(article => (
-              <Column sm={4} md={4} lg={8} key={article.route}>
-                <ArticleEntry article={article} />
-              </Column>
-            ))}
-          </Row>
+          <ArticleEntryList
+            type={hasSubCategories ? "oneColumn" : "twoColumns"}
+            articles={articles}
+          />
         </>
       ) : null}
     </>
