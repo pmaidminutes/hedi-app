37debbc719dee097e3d315fd2d4ad3b5d74c0686
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-24 12:01:11 +0200

    [article-entry-list] mod ArticleEntry * use BreadCrumb * use light ClickableTile

--- modules/editorial/article/client/components/ArticleEntry/transformArticleEntry.ts
+++ modules/editorial/article/client/components/ArticleEntry/transformArticleEntry.ts
@@ -1,10 +1,33 @@
+import { IRouteLabeled } from "@/modules/model";
+import { BreadcrumbType } from "@/modules/shell/client/components/BreadCrumb/transformBreadCrumb";
 import { IArticleEntry } from "../../../types";
 
 export interface IArticleEntryProps {
   article: IArticleEntry;
+  withGraphicalBreadcrumb?: boolean;
 }
-export function transformArticleEntry({ article }: IArticleEntryProps) {
-  const { label, route } = article;
+export function transformArticleEntry({
+  article,
+  withGraphicalBreadcrumb,
+}: IArticleEntryProps) {
+  const { label, route, type, lang, routelabel, appStyle } = article;
 
-  return { label, route };
+  const breadcrumbType: BreadcrumbType =
+    withGraphicalBreadcrumb !== undefined && withGraphicalBreadcrumb === true
+      ? "graphical"
+      : "withoutTitle";
+  // TODO check if breadcrumb type works here for all cases
+  return {
+    label,
+    route,
+    breadcrumbData: {
+      label,
+      routelabel,
+      route,
+      type,
+      lang,
+      appStyle,
+      breadcrumbType,
+    },
+  };
 }
