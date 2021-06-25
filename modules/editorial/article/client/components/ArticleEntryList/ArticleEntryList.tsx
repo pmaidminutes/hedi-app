ebd5fc636120fd94f5aba96ce9be1dfcf33e4018
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-25 10:48:44 +0200

    [mfa] add headline to ArticleEntryList

--- modules/editorial/article/client/components/ArticleEntryList/ArticleEntryList.tsx
+++ modules/editorial/article/client/components/ArticleEntryList/ArticleEntryList.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Row, Column } from "carbon-components-react";
-import { IArticleEntry } from "../../../types";
 import { ArticleEntry } from "@/modules/editorial/article/client/components";
-import { transformArticleEntryList } from "./transformArticleEntryList";
-
-export interface IArticleEntryList {
-  articles: IArticleEntry[];
-  type?: "oneColumn" | "twoColumns";
-}
+import {
+  transformArticleEntryList,
+  IArticleEntryList,
+} from "./transformArticleEntryList";
 
 export const ArticleEntryList = (props: IArticleEntryList) => {
-  const { articles, type } = transformArticleEntryList(props);
+  const { articles, type, headline } = transformArticleEntryList(props);
   if (type === "oneColumn") {
     return (
       <>
+        <Row>
+          <Column>
+            <h3>{headline}</h3>
+          </Column>
+        </Row>
         {articles.map(article => (
           <Row>
             <Column sm={4} md={4} lg={8} key={article.route}>
