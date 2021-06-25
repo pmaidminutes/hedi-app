81b8cf2ae033036c23437ccc40cca34b36487288
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-25 10:27:15 +0200

    [article-teaser] mod Image to use layout

--- modules/components/client/components/Image/transformImage.ts
+++ modules/components/client/components/Image/transformImage.ts
@@ -1,12 +1,61 @@
 import { PartialBy } from "@/modules/common/utils";
+import { ImageProps } from "next/image";
 import { IImageComponent } from "../../../types";
 
-export type IImageProps = PartialBy<IImageComponent, "kind" | "usage">;
+export type IImageProps = PartialBy<IImageComponent, "kind" | "usage"> &
+  Omit<ImageProps, "src">;
 
 export function transformImage(props: IImageProps) {
-  const { kind, usage, label, route, ...rest } = props;
-  return {
+  const {
+    kind,
+    usage,
+    label,
+    route,
+    layout,
+    width,
+    height,
+    alt,
+    className,
+    sizes,
+    quality,
+    priority,
+    placeholder,
+    objectFit,
+    objectPosition,
+    loading,
+    blurDataURL,
+    unoptimized,
+
+    ...rest
+  } = props;
+
+  const imageProps = {
+    alt,
+    className,
     src: process.env.NEXT_PUBLIC_ASSETS_URL + route,
-    ...rest,
+    sizes,
+    quality,
+    priority,
+    objectFit,
+    objectPosition,
+    loading,
+    unoptimized,
   };
+
+  // TODO: check why placeholder does not work
+
+  switch (layout) {
+    case "fill":
+      return {
+        layout,
+        ...imageProps,
+      };
+    default:
+      return {
+        layout,
+        width,
+        height,
+        ...imageProps,
+      };
+  }
 }
