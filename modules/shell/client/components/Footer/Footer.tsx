bd7965489f9e151f8cc4b70ff1db10b918b63da5
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-27 13:08:17 +0200

    [fe-adjustments] Footer and shell adjusmtents

--- modules/shell/client/components/Footer/Footer.tsx
+++ modules/shell/client/components/Footer/Footer.tsx
@@ -3,31 +3,39 @@ import { transformFooter, IFooter } from "./transformFooter";
 import { Link } from "carbon-components-react";
 import { LanguageSwitch } from "../LanguageSwitch";
 import { isLink } from "@/modules/components/types";
+import { Button } from "@/modules/components";
 
 export const Footer = (props: IFooter) => {
   const { footer, languageSwitchLinks, shellConfig } = transformFooter(props);
   return (
     <footer aria-label="footer" className="hedi--footer">
-      {footer &&
-        footer.map((link: any, index: number) => {
-          if (isLink(link)) {
-            return (
-              <Link
-                key={link.labelText + index}
-                href={link.href}
-                title={link.labelText}>
-                {link.labelText}
-              </Link>
-            );
-          }
-        })}
-      {languageSwitchLinks ? (
-        <LanguageSwitch
-          links={languageSwitchLinks}
-          direction="top"
-          config={shellConfig}
-        />
-      ) : null}
+      <nav className="hedi--footer__nav">
+        <ul aria-label="Footer Navigation" className="hedi--footer__menu-bar">
+          {footer &&
+            footer.map((link: any, index: number) => {
+              if (isLink(link)) {
+                return (
+                  <li key={link.labelText + index}>
+                    <Link
+                      href={link.href}
+                      title={link.labelText}
+                      tabIndex={0}
+                      className="hedi--footer__menu-item">
+                      {link.labelText}
+                    </Link>
+                  </li>
+                );
+              }
+            })}
+          {languageSwitchLinks ? (
+            <LanguageSwitch
+              links={languageSwitchLinks}
+              direction="top"
+              config={shellConfig}
+            />
+          ) : null}
+        </ul>
+      </nav>
     </footer>
   );
 };
