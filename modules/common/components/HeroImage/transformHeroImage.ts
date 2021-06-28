a1da702bbd5689713aa05d4b45e03a5448209514
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-28 12:16:13 +0200

    [objectFit-ArticleImage] use ratio to set objectFit and position

--- modules/common/components/HeroImage/transformHeroImage.ts
+++ modules/common/components/HeroImage/transformHeroImage.ts
@@ -1,13 +1,25 @@
 import { IImageProps } from "@/modules/components";
 
 export function transformHeroImage(image: IImageProps) {
-  const { color, layout, objectFit, objectPosition } = image;
+  const { color, layout, objectFit, objectPosition, width, height } = image;
+
+  const ratio: number = width / height;
+  const transformedObjectfit = objectFit || getObjectFitByRatio(ratio);
+  const transformedObjectPos =
+    objectPosition || getObjectPositionByRatio(ratio);
 
   return {
     color: color || "transparent",
     image,
     layout: layout || "fill",
-    objectFit: objectFit || "cover",
-    objectPosition: objectPosition || "center center",
+    objectFit: transformedObjectfit,
+    objectPosition: transformedObjectPos,
   };
+}
+
+function getObjectFitByRatio(ratio: number) {
+  return ratio > 1.8 ? "cover" : "scale-down";
+}
+function getObjectPositionByRatio(ratio: number) {
+  return ratio > 1.8 ? "top center" : "center";
 }
