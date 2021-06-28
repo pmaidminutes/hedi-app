5746003765c5b65e4c0dc82aec1db403d1192581
Nico Putzehl <nputzehl@aidminutes.com>
2021-06-28 22:30:34 +0200

    [actionbar-actions] get actions from cms

--- modules/editorial/article/client/components/Article/transformArticle.ts
+++ modules/editorial/article/client/components/Article/transformArticle.ts
@@ -1,5 +1,7 @@
 import {
   findAllHeadlineInstances,
+  findGroupInstance,
+  findLabelInstance,
   findLinkInstance,
   ILinkComponent,
 } from "@/modules/components/types";
@@ -20,34 +22,46 @@ export function transformArticle(props: IArticleProps) {
   } = content;
 
   const headlines = findAllHeadlineInstances(components);
-
-  const handleShare = () => console.log("SHARE");
-  const handleBookmark = () => console.log("BOOKMARK");
-
   const backLink = findLinkInstance(uiComponents, "backLink");
+  const actionBar = findGroupInstance(uiComponents, "actionbar");
 
-  const actions: IActionBarAction[] = [
-    {
+  let audio,
+    share,
+    print,
+    language = undefined;
+
+  if (actionBar?.components) {
+    audio = findLabelInstance(actionBar.components, "audio");
+    share = findLabelInstance(actionBar.components, "share");
+    print = findLabelInstance(actionBar.components, "print");
+    language = findLabelInstance(actionBar.components, "language");
+  }
+
+  const actions: IActionBarAction[] = [];
+  if (audio)
+    actions.push({
       type: "audio",
-      description: "Artikel hören",
+      description: audio.text || "",
       handler: () => {},
-    },
-    {
+    });
+  if (share)
+    actions.push({
       type: "share",
-      description: "Teilen",
+      description: share.text || "",
       handler: () => {},
-    },
-    {
+    });
+  if (print)
+    actions.push({
       type: "print",
-      description: "Drucken",
+      description: print.text || "",
       handler: () => {},
-    },
-    {
+    });
+  if (language)
+    actions.push({
       type: "language",
-      description: "Sprache ändern",
+      description: language.text || "",
       handler: () => {},
-    },
-  ];
+    });
 
   // TODO hier die links bauen
   const anchors: ILinkComponent[] = headlines.map(headline => {
