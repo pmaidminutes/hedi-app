1ca3405e56193faff1c862911d5ecd7338757b26
woeishi <woei@refect.net>
2021-06-30 19:55:06 +0200

    [ops] dedicated env file for appstaging, deploy renames remote

--- deploy-appstaging.sh
+++ deploy-appstaging.sh
@@ -2,7 +2,9 @@
 
 function sync_files() {
     echo "synchronizing config files"
-    rsync -avzP .babelrc .env.local next.config.js package.json yarn.lock root@cms.projekt-hedi.de:/root/aidminutes/hedi-app/hedi-app
+    rsync -avzP .babelrc .env.appstaging next.config.js package.json yarn.lock root@cms.projekt-hedi.de:/root/aidminutes/hedi-app/hedi-app
+    echo "rename env file"
+    ssh root@cms.projekt-hedi.de cp aidminutes/hedi-app/hedi-app/.env.appstaging aidminutes/hedi-app/hedi-app/.env.local
     echo "synchronizing data"
     rsync -avzP .next root@cms.projekt-hedi.de:/root/aidminutes/hedi-app/hedi-app
 }
